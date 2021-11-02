f99026aada225d0318818576feaeb28ccfde713a
Nidhi Kumari <nknidhi321@gmail.com>
2021-11-02 17:20:35 +0530

    ==========IMPORTANT============

--- binary-tree/_1_node-to-root-path-binary-tree.java
+++ binary-tree/_1_node-to-root-path-binary-tree.java
@@ -14,15 +14,55 @@ public class Main {
       this.val = val;
     }
   }
-
-  //=========================================================================================
+  
+ //=================================================================================================================
 
   //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
   //$$$$$$$$$$$$$$$===============================$$$$$$$$$$$$$$$$$$$$$$$$$
   //$$$$$$$$$$$$$$$==========IMPORTANT============$$$$$$$$$$$$$$$$$$$$$$$$$ 
   //$$$$$$$$$$$$$$$===============================$$$$$$$$$$$$$$$$$$$$$$$$$
   //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
-  public static ArrayList<TreeNode> nodeToRootPath(TreeNode root, int data) {
+	
+  public static boolean nodeToRootPath_01(TreeNode root, int data, ArrayList<TreeNode> ans) {
+    // write your code here
+		if(root == null) return false;
+		
+		if(root.val == data) {
+			ans.add(root);
+			return true;
+		}		
+		if(nodeToRootPath_01(root.left, data, ans)) {
+			ans.add(root);
+			return true;
+		}
+		if(nodeToRootPath_01(root.right, data, ans)) {
+			ans.add(root);
+			return true;	
+		}
+		return false;
+	}
+
+  public static void nodeToRootPath_02(TreeNode root, int data, ArrayList<TreeNode> ans) {
+	// write your code here
+		if(root == null) return;
+		
+		if(root.val == data) {
+			ans.add(root);
+			return;
+		}		
+		nodeToRootPath_02(root.left, data, ans);
+		if(ans.size() > 0) {
+			ans.add(root);
+			return;
+		}
+		nodeToRootPath_02(root.right, data, ans);
+		if(ans.size() > 0) {
+			ans.add(root);
+			return;	
+		}
+	}
+	
+  public static ArrayList<TreeNode> nodeToRootPath_03(TreeNode root, int data) {
     // write your code here
     if(root == null) return new ArrayList<>();
 		
@@ -31,20 +71,20 @@ public class Main {
 		ans.add(root);
 		return ans;
 	}		
-	ArrayList<TreeNode> leftSmallAns = nodeToRootPath(root.left, data);
+	ArrayList<TreeNode> leftSmallAns = nodeToRootPath_03(root.left, data);
 	if(leftSmallAns.size() > 0) {
 		leftSmallAns.add(root);
 		return leftSmallAns;
 	}
-	ArrayList<TreeNode> rightSmallAns = nodeToRootPath(root.right, data);
+	ArrayList<TreeNode> rightSmallAns = nodeToRootPath_03(root.right, data);
 	if(rightSmallAns.size() > 0) {
 		rightSmallAns.add(root);
 		return rightSmallAns;	
 	}
 	return new ArrayList<>();
   }
-
-  // ==========================================================================================
+  
+  //===========================================================================================================
 
   public static TreeNode createTree(int[] arr, int[] IDX) {
     if (IDX[0] > arr.length || arr[IDX[0]] == -1) {
@@ -68,11 +108,22 @@ public class Main {
     TreeNode root = createTree(arr, IDX);
 
     int data = scn.nextInt();
-    ArrayList<TreeNode> ans = nodeToRootPath(root, data);
+    
+    // ArrayList<TreeNode> ans = new ArrayList<>();
+    // if(!nodeToRootPath_01(root, data, ans)) System.out.println();
+    // for(TreeNode node : ans)
+    // System.out.print(node.val + " ");
+    
+    // ArrayList<TreeNode> ans = new ArrayList<>();
+    // nodeToRootPath_02(root, data, ans);
+    // if (ans.size() == 0)  System.out.println();
+    // for (TreeNode node : ans)
+    //   System.out.print(node.val + " ");
+      
+    ArrayList<TreeNode> ans = nodeToRootPath_03(root, data);
     if (ans.size() == 0)  System.out.println();
     for (TreeNode node : ans)
       System.out.print(node.val + " ");
-
   }
 
   public static void main(String[] args) {
