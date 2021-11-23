cc439e4fe53a100ffb5e98f769d7eade3a6d3815
Nidhi Kumari <nknidhi321@gmail.com>
2021-11-24 01:03:42 +0530

    Create _3.1_is-graph-bipartite.java

---
+++ graph/_3.1_is-graph-bipartite.java
@@ -0,0 +1,110 @@
+//https://leetcode.com/problems/is-graph-bipartite/
+
+/*
+  NOTE: If every vertex of graph is in connected component then :
+    1) Every non cyclic graph is bipartite, you can check this by BFS traversal on that graph.
+    2) In every cyclic graph :
+         a) If total number of vertex in a graph is even, then the graph is bipartite.
+         b) If total number of vertex in a graph is odd, then the graph is not bipartite.*
+
+    Here, the idea is to Mark source vertex with 0 and all it's neighbour with 1, and again mark neighbours neighbour with 0 and so on..
+    If you find any vertex which is already visited and is marked with say 0 and you want to mark it with 1 then we can say graph is not bipartite.
+    In simple terms, If there is conflict in marking a vertex then it is not bipartite.
+*/
+
+//Mine // BFS
+//Checking visited and color before adding in the queue
+//More efficient
+
+class Solution {
+    public boolean isBipartite(int[][] graph) {
+
+        int[] visitedOrMarkedColor = new int[graph.length];
+        Arrays.fill(visitedOrMarkedColor, -1);
+        
+        for(int i = 0; i < graph.length; i++) {
+            if(visitedOrMarkedColor[i] == -1 && !isComponentBipartite(i, visitedOrMarkedColor, graph)) {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    public static boolean isComponentBipartite(int src, int[] visitedOrMarkedColor, int[][] graph) {
+       
+        Queue<Integer> queue = new LinkedList<Integer>();
+        queue.offer(src);
+        visitedOrMarkedColor[src] = 0;
+        
+        int color = 1;
+        while(!queue.isEmpty()) {
+            int size = queue.size();
+            while(size-- > 0) {
+                int curr = queue.poll();
+                
+                for(int neigh : graph[curr]) {                        
+                    if(visitedOrMarkedColor[neigh] != -1 && visitedOrMarkedColor[neigh] != color) {
+                        return false;
+                    }
+                    if(visitedOrMarkedColor[neigh] == -1) {
+                        queue.offer(neigh);
+                        visitedOrMarkedColor[neigh] = color;
+                    }
+                }
+            }
+            color = (color + 1) % 2;
+        }
+        return true;
+    }
+}
+
+//XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+
+//Rajneesh //BFS
+//Checking visited and color after adding in the queue
+
+class Solution {
+    public boolean isBipartite(int[][] graph) {
+
+        int[] visitedOrMarkedColor = new int[graph.length];
+        Arrays.fill(visitedOrMarkedColor, -1);
+        
+        for(int i = 0; i < graph.length; i++){
+            if(visitedOrMarkedColor[i] == -1 && !isComponentBipartite(i, visitedOrMarkedColor, graph)){
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    public static boolean isComponentBipartite(int src, int[] visitedOrMarkedColor, int[][] graph){
+       
+        Queue<Integer> queue = new LinkedList<Integer>();
+        queue.offer(src);
+      
+        int color = 0;
+        while(!queue.isEmpty()){
+            int size = queue.size();
+            while(size-- > 0){
+                int curr = queue.poll();
+                
+                if(visitedOrMarkedColor[curr] != -1) {
+                    if(visitedOrMarkedColor[curr] != color){
+                        return false;
+                    }
+                    continue;
+                }
+                
+                visitedOrMarkedColor[curr] = color;
+                for(int neigh : graph[curr]){
+                    if(visitedOrMarkedColor[neigh] == -1){
+                        queue.offer(neigh);
+                    }
+                }
+            }
+            color = (color + 1) % 2;
+        }
+        return true;
+    }
+}
+//XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
