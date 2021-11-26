97bbaf7282e6aa7735c4ab9d30dfabd9474c3d3b
Nidhi Kumari <nknidhi321@gmail.com>
2021-11-26 21:07:00 +0530

    Update _3.1.2_possible-bipartition.java

--- graph/_3.1.2_possible-bipartition.java
+++ graph/_3.1.2_possible-bipartition.java
@@ -15,6 +15,8 @@ class Solution {
     public boolean possibleBipartition(int n, int[][] dislikes) {
         
         // graph ranges from [1 - n], so size will be n + 1, where graph[0] will be a waste 
+		
+	// Graph creation
         ArrayList<Integer>[] graph = new ArrayList[n + 1];
         for(int i = 0; i <= n; i++) {
             graph[i] = new ArrayList<>();
@@ -24,8 +26,9 @@ class Solution {
             graph[dislike[0]].add(dislike[1]);
         }
         
+	// Keeping visited and marked color in the  same array
         int[] visOrColor = new int[n + 1];
-        Arrays.fill(visOrColor, -1);
+        Arrays.fill(visOrColor, -1); // Initially marking all nodes as unvisited 
         
         for(int i = 1; i <= n; i++) {
             if(visOrColor[i] == -1 && !isComponentBipartite(i, visOrColor, graph)) {  //Check for all components
@@ -39,19 +42,21 @@ class Solution {
         Queue<Integer> queue = new LinkedList<>();
         queue.add(src);
         
-        int color = 0;
+        int color = 0; // Source node color will be Red
         while(!queue.isEmpty()) {
             int size = queue.size();
             while(size-- > 0) {
                 int currVtx = queue.poll();
 				
-				// Already visited
+		// Already visited
                 if(visOrColor[currVtx] != -1) { 
-                    if(visOrColor[currVtx] != color) { // Already visited with different color => Conflict
+                    if(visOrColor[currVtx] != color) { // Already visited with different color => Conflict => Odd edge cycle
                         return false; // That's the end return 
                     }
-					// Already visited with same color => No conflict => Further process your BFS   
-                    continue; // Don't forget this line else it will increase the complexity
+					
+		    // Already visited with same color => No conflict => Even edge cycle => Further process your BFS   
+                    // NOTE : The already visited nodes have already added it's nbr so why to waste the below for loop check, so continue
+		    continue; // Don't forget this line else it will increase the complexity		
                 }
                 
                 visOrColor[currVtx] = color; // Marking visited or assigned color
