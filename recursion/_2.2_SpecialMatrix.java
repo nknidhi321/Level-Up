6840acbf6abac107f718a072e0f7f74a26c3ceeb
Nidhi Kumari <nknidhi321@gmail.com>
2021-12-19 18:32:35 +0530

    Update _2.2_SpecialMatrix.java

--- recursion/_2.2_SpecialMatrix.java
+++ recursion/_2.2_SpecialMatrix.java
@@ -1,20 +1,27 @@
-//https://practice.geeksforgeeks.org/problems/special-matrix4201/1#
+// https://practice.geeksforgeeks.org/problems/special-matrix4201/1#
+
+// Memoization
 
 class Solution{
+    
    public int FindWays(int n, int m, int[][] blocked_cells){
+	
+	// Creating arr matrix to block the given blocked cells 
         int[][] arr = new int[n][m];
+	   
+	// Blocking the cells of the matrix
         for(int row = 0; row < blocked_cells.length; row++){
             int x = blocked_cells[row][0] - 1; //1 based index
             int y = blocked_cells[row][1] - 1; //1 based index
             arr[x][y] = 1;
         }
         
+        // When starting box or ending box is blocked => No paths
         if(arr[0][0] == 1 || arr[n - 1][m - 1] == 1) return 0;
         
-        // Integer[][] dp = new Integer[n][m];
-        // return FindWays_Memo(0, 0, arr, dp); 
+        Integer[][] dp = new Integer[n][m];
+        return FindWays_Memo(0, 0, arr, dp); 
         
-        return FindWays_Tab(0, 0, arr);
     }
     
     public static int M = 1000000007;
@@ -37,10 +44,33 @@ class Solution{
         return dp[sr][sc] = count;
     }
     
-  
-    public static int FindWays_Tab(int SR, int SC, int[][] arr){
+}
+
+//XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+
+// Tabulation
+
+class Solution{
+    
+   public int FindWays(int n, int m, int[][] blocked_cells){
+        int[][] arr = new int[n][m];
+        for(int row = 0; row < blocked_cells.length; row++){
+            int x = blocked_cells[row][0] - 1; //1 based index
+            int y = blocked_cells[row][1] - 1; //1 based index
+            arr[x][y] = 1;
+        }
+        
+        if(arr[0][0] == 1 || arr[n - 1][m - 1] == 1) return 0;
         
         int[][] dp = new int[arr.length][arr[0].length];
+        
+        return FindWays_Tab(0, 0, arr, dp);
+    }
+    
+    public static int M = 1000000007;
+    public static int[][] dir = {{0, 1}, {1, 0}};
+  
+    public static int FindWays_Tab(int SR, int SC, int[][] arr, int[][] dp){
         
         for(int sr = arr.length - 1; sr >= 0; sr--){
             for(int sc = arr[0].length - 1; sc >= 0; sc--){
@@ -63,4 +93,6 @@ class Solution{
         }
         return dp[SR][SC];
     }
+    
 }
+//XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
