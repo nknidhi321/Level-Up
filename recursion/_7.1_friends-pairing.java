c08c67f0c8ee4d7ae73be151aed27065f8ce2201
Nidhi Kumari <nknidhi321@gmail.com>
2021-12-27 22:24:16 +0530

    Update _7.1_friends-pairing.java

--- recursion/_7.1_friends-pairing.java
+++ recursion/_7.1_friends-pairing.java
@@ -1,46 +1,66 @@
-//https://www.pepcoding.com/resources/online-java-foundation/dynamic-programming-and-greedy/friends-pairing-official/ojquestion
+// https://www.pepcoding.com/resources/data-structures-and-algorithms-in-java-levelup/recursion-and-backtracking/friends-pairing-2-official/ojquestion
 
-//People on level
-//Always pass idx + 1
+// People on level
+// Always pass idx + 1 in next call
+
+// Har call untimately n tak pahuchegi, doesn't matter if you have already formed your answer, 
+// You will be propargating to the next call -> then next -> .... until you reach n, so base case that is idx == n pe hamesha reach karoge aap
+// And jis din reach kar jaaoge idx == n pe => Ek tareeka mil gaya
+
+// Isme idx k terms me answer bnana, par friends-pairing dp wale question me n k terms me answer bnana.
+
+// Isme idx k terms me answer q bnana ? Kuki loop lgani h, and apne se aage wale k saath he aap pair bna saktey ho jo display v karni hai
+// Remember to generate combination and not permuatiom
 
 import java.io.*;
 import java.util.*;
 
 public class Main {
+    
+   public static int serialNo = 1;
 
-    public static void main(String[] args) throws Exception {
+   public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         boolean[] visited = new boolean[n];
-        System.out.println(pairing(0, n, visited));
-    }
+        pairing_Rec(0, n, visited, "");
+   }
+  
     
-    public static int pairing(int idx, int n, boolean[] visited) {
-        if(idx == n) return 1;
+   // Levels pe har ek frnd hoga ek baar, pehle 0, 1, 2, so on...
+   public static int pairing_Rec(int idx, int n, boolean[] visited, String ans) {
+        if(idx == n)  {
+            System.out.println(serialNo++ + "." + ans);
+            return 1; // Sbko process kar liya => Ek tareeka mil gaya 
+        }
         
-        int count = 0;
+        int count = 0; // Total number of ways
         
-        //Your fate is already decided by someone, aage badh jaao
-        if(visited[idx]) count += pairing(idx + 1, n, visited);
+        // Your fate is already decided by someone, aage badh jaao,
+        // someone already made pair with you from prev calls
+        if(visited[idx]) count += pairing_Rec(idx + 1, n, visited, ans);
         
+        // Tumhe kvi kisi ne nahi choona => Ab tumhare pass 2 choice hai 1)akele jaao 2) Kisi k saath jaao
         else {
-            visited[idx] = true;
+            visited[idx] = true; // Mark yourself as visited
             
-            //Akele jaane ki choice
-            count += pairing(idx + 1, n, visited);
+            // Akele jaane ki choice
+            count += pairing_Rec(idx + 1, n, visited, ans + "(" + (idx + 1) + ") ");
             
-            //Kisi k saath jaane ki choice
-            for(int i = idx + 1; i < n; i++) { //We don't have to generate permutation so make sure not to start loop from 0
-                if(!visited[i]) {
-                    visited[i] = true;
-                    count += pairing(idx + 1, n, visited);  //idx + 1 will be passed and not i + 1, because we have individual level for each people
-                    visited[i] = false;
+            // Kisi k saath jaane ki choice
+            for(int i = idx + 1; i < n; i++) {
+                if(!visited[i]) { // Jiske saath jaana chahte ho wo visited nahi h
+                    visited[i] = true; // Toh tum uske saath chale jaao
+                    
+                    // Here idx + 1 he pass hoga kuki har ek ka apna level hoga 0, 1, 2, so on..
+                    count += pairing_Rec(idx + 1, n, visited, ans + "(" + (idx + 1) + "," + (i + 1) + ") "); 
+                    visited[i] = false; // Backtrack the frnd, jiske saath gaye the
                 }
             }
-            visited[idx] = false;
+            visited[idx] = false; // Backtrack, unmark yourself
         }
-        return count;
+        return count; // Jitne v tareeke aaye uska count 
     } 
-
+    
 }
 //XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
