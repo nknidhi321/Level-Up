0a35a9e552e8776b4c59454b53af27eb329da3fd
Nidhi Kumari <nknidhi321@gmail.com>
2022-01-04 21:06:02 +0530

    Create _Archit_2.4_count-the-triplets.java

---
+++ TwoPointer/_Archit_2.4_count-the-triplets.java
@@ -0,0 +1,41 @@
+// https://practice.geeksforgeeks.org/problems/count-the-triplets4615/1#
+
+/*
+    Koi 2 number kisi tisre number k equal kab hoga ??
+    
+    Jab "2 chote" numbers ko add karenge tvi kisi "tisre bade" number ko achieve kar paaenge hum
+    
+    Isliye loop piche se lgao kuki array sorted rahega, so target hamesha ith element ko bnaenge
+    and two pointer [0 to i-1] pe lga denge jiski range obviously ith element se choti hogi
+
+*/
+
+class Solution {
+    
+    public int countTriplet(int nums[], int n) {
+        Arrays.sort(nums);
+        int count = 0;
+        for(int i = n - 1; i >= 2; i--) {
+            count += twoSum(0, i - 1, nums[i], nums);
+        }
+        return count;
+    }
+    
+    public int twoSum(int start, int end, int target, int[] nums) {
+        // Two pointer
+        int count = 0;
+        while(start < end) {
+            int sum = nums[start] + nums[end];
+            if(sum == target) {
+                count++;
+                start++;
+                end--;
+            }
+            else if(sum < target) start++;
+            else end--;
+        }
+        return count;
+    }
+}
+
+//XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
