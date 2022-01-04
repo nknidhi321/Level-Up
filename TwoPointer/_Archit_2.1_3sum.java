b8767b389c9aa0b46b98a590f1c9ead312e27246
Nidhi Kumari <nknidhi321@gmail.com>
2022-01-04 12:28:32 +0530

    Update _Archit_2.1_3sum.java

--- TwoPointer/_Archit_2.1_3sum.java
+++ TwoPointer/_Archit_2.1_3sum.java
@@ -1,7 +1,6 @@
 // https://leetcode.com/problems/3sum/
 
 // Two Pointer 
-// Mine
 // TC : O(N^2)
 
 class Solution {
@@ -14,13 +13,13 @@ class Solution {
         if(n < 3) return ansList;
         
         Arrays.sort(nums);
-        for(int i = 0; i <= n - 3; i++) { // Fixing the ith element and calling 2 pointer from i+1 to n-1
+        for(int i = 0; i <= n - 3; i++) { // Fixing the ith element and calling 2 pointer from [i+1 to n-1]
             if(i != 0 && nums[i] == nums[i - 1]) continue; // Skipping duplicates
             
             int target = 0 - nums[i];
             List<List<Integer>> lists = twoSum(i + 1, n - 1, target, nums);
-            for(List<Integer> list : lists){
-                list.add(nums[i]);
+            for(List<Integer> list : lists) {
+                list.add(0, nums[i]); // Shifting will take place, since adding at 0th idx in ArrayList
                 ansList.add(list);
             }
         }
@@ -29,24 +28,26 @@ class Solution {
     
     public static List<List<Integer>> twoSum(int start, int end, int target, int[] nums) {
         List<List<Integer>> ans = new ArrayList<>();
-        
+        int si = start;
+	    
         // Two Pointer
         while(start < end) {
+            
+	    // Since unique pairs is asked so surpass all the elements from start which are same
+            if(start != si && nums[start] == nums[start - 1]) {
+                start++;
+                continue;
+            }
+		
+	    // NOTE : end duplicates will be managed implicitly, because now sum will change, so end will decrement someday accordingly.  
+		
             int sum = nums[start] + nums[end];
 	    
-	        // Got one of the target
-            if(sum == target) {
+	        if(sum == target) { // Got one of the target
                 List<Integer> asf = new ArrayList<>();
                 asf.add(nums[start]); asf.add(nums[end]);
                 ans.add(asf);
-                
-                // Since unique pairs is asked so surpass all the elements from end which are same   
-                while(end - 1 >= 0 && (nums[end - 1] == nums[end])) end--;
-                end--;
-              
-                // Since unique pairs is asked so surpass all the elements from start which are same  
-                while(start + 1 < n && (nums[start + 1] == nums[start])) start++;
-                start++;
+                start++; end--;
             }
             else if(sum < target) start++;
             else end--;
