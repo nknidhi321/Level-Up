9a7d13e68bc7693162230ff75ffaf6c8becd8936
Nidhi Kumari <nknidhi321@gmail.com>
2022-01-06 02:17:20 +0530

    Update _Archit_3.1_4-sum.java

--- TwoPointer/_Archit_3.1_4-sum.java
+++ TwoPointer/_Archit_3.1_4-sum.java
@@ -1,6 +1,8 @@
 // https://leetcode.com/problems/4sum/
 
+// 4 Sum
 // Seeresh
+// Check down for recursive Sol.
 
 class Solution {
     
@@ -31,6 +33,64 @@ class Solution {
             if(i != si && nums[i] == nums[i - 1]) continue; // Skipping duplicates
             
             List<List<Integer>> lists = twoSum(i + 1, n - 1, target - nums[i], nums);
+            for(List<Integer> list : lists) {
+                list.add(0, nums[i]); // Shifting will take place, since adding at 0th idx in ArrayList
+                ansList.add(list);
+            }
+        }
+        return ansList;
+    }
+    
+    public static List<List<Integer>> twoSum(int start, int end, int target, int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        int si = start;
+        // Two Pointer
+        while(start < end) {
+            // Since unique pairs is asked so surpass all the elements from start which are same
+            if(start != si && nums[start] == nums[start - 1]) {
+                start++;
+                continue;
+            }
+            int sum = nums[start] + nums[end];
+	    
+	        if(sum == target) { // Got one of the target
+                List<Integer> asf = new ArrayList<>();
+                asf.add(nums[start]); asf.add(nums[end]);
+                ans.add(asf);
+                start++; end--;
+            }
+            else if(sum < target) start++;
+            else end--;
+        }
+        return ans;
+    }
+}
+
+//XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+
+// // Recursive k Sum, where k = 4
+
+class Solution {
+    
+    public static int n;
+    
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        List<List<Integer>> ansList = new ArrayList<>();
+        n = nums.length;
+        if(n < 4) return ansList;
+        
+        Arrays.sort(nums);
+        return kSum(0, target, 4, nums);
+    }
+        
+    public static List<List<Integer>> kSum(int si, int target, int k, int[] nums) {
+        if(k == 2) return twoSum(si, n - 1, target, nums);
+        
+        List<List<Integer>> ansList = new ArrayList<>();
+        for(int i = si; i <= n - k; i++) { // Fixing the ith element and calling (k-1)Sum from [i+1 to n-1]
+            if(i != si && nums[i] == nums[i - 1]) continue; // Skipping duplicates
+            
+            List<List<Integer>> lists = kSum(i + 1, target - nums[i], k - 1, nums);
             for(List<Integer> list : lists) {
                 list.add(0, nums[i]); // Shifting will take place, since adding at 0th idx in ArrayList
                 ansList.add(list);
