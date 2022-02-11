6592b5571004bf74f516c3df5e0ea66843d86ffb
Nidhi Kumari <nknidhi321@gmail.com>
2022-02-11 16:18:03 +0530

    Update validate-stack-sequences.java

--- stacks-and-queues/validate-stack-sequences.java
+++ stacks-and-queues/validate-stack-sequences.java
@@ -6,13 +6,23 @@ class Solution {
         int n = pushed.length;
         int j = 0;
         Stack<Integer> stack = new Stack<>();
+        
         for(int i = 0; i < n; i++) {
             stack.push(pushed[i]);
-            while(!stack.isEmpty() && stack.peek() == popped[j]) {
+        
+            // Jab v push karo toh stack me check karo ki kya stack k top pe wahi element hai
+            // jisko hame pop karwana hai popped array se check kar k, if yes then pop
+            
+            while(!stack.isEmpty() && stack.peek() == popped[j]) {  
                 stack.pop();
                 j++;
             }
         }
-        return stack.isEmpty();    
+        
+        // Agar sbko push and krwane k baad stack empty hai => Validated (Possible)
+        // Ya fir ye dekh lo ki jth pointer n tak pahuch paaya ya nahi => Agar nahi pahuch paaya that means
+        // stack me ab v kuch elements hai jo ki pop nahi ho paaenge popped array k sequence me
+        
+        return stack.isEmpty();    // return j == n;
     }
 }
