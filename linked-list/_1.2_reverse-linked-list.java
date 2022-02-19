cdf289e1511457e86720686bd777a5468968f94e
Nidhi Kumari <nknidhi321@gmail.com>
2022-02-19 10:49:41 +0530

    Create _1.2_reverse-linked-list.java

---
+++ linked-list/_1.2_reverse-linked-list.java
@@ -0,0 +1,60 @@
+// https://leetcode.com/problems/reverse-linked-list/
+Reverse pointers of LL
+
+Rajneesh Bhaiya
+Awesome analogy for forward(just a temp node inside loop)
+
+```
+class Solution {
+    
+    public ListNode reverseList(ListNode head) {
+        if(head == null || head.next == null) return head;  // Aadat
+        
+        ListNode prev = null;
+        ListNode curr = head;
+        
+        while(curr != null) {
+            ListNode forward = curr.next; // Break krne se pehle aage k nodes ko point karwa lo kisi se [backup]
+            curr.next = prev;             // curr k next me prev node ka address daal do, Reverse ho gayi [link]
+            
+            prev = curr;                  // next iteration k liye prev curr pe chala jaaega
+            curr = forward;               // aur curr forward pe chala jaaega 
+        }
+        
+        // NOTE : forward bus ek temp backup node tha loop me
+        
+        return prev;
+    }
+}
+```
+
+---------------------------------------------------------------------------------------------------------------------
+
+Mine, bekar analogy of forward
+
+```
+class Solution {
+    
+    public ListNode reverseList(ListNode head) {
+        if(head == null || head.next == null) return head;
+        
+        ListNode prev = null;
+        ListNode curr = head;
+        ListNode forward = head;
+        
+        // * Kisi v point pe prev previous pe point karega, par curr aur forward same node(prev k next) ko point karega
+        // * While loop k andar ye variables apne naam jaise kaam karenge
+        
+        while(curr != null) {
+            forward = curr.next; // Break krne se pehle aage k nodes ko point karwa lo kisi se [backup]
+            curr.next = prev;    // curr k next me prev node ka address daal do, Reverse ho gayi [link]
+            
+            prev = curr;         // next iteration k liye prev curr pe chala jaaega
+            curr = forward;      // * aur curr forward pe chala jaaega 
+        }
+        return prev;
+    }
+}
+```
+
+-------------------------------------------------------------------------------------------------------------------------
