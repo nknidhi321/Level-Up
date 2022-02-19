fde40167601cd6f9c33eaad46f9deac4f2818163
Nidhi Kumari <nknidhi321@gmail.com>
2022-02-19 14:00:11 +0530

    Update _1.3_palindrome-linked-list.java

--- linked-list/_1.3_palindrome-linked-list.java
+++ linked-list/_1.3_palindrome-linked-list.java
@@ -1,19 +1,29 @@
 // https://leetcode.com/problems/palindrome-linked-list/
 
+Intuition : 2nd half k ptrs. ko reverse kar lo and check for palindrome
+NOTE : head ko hamesha intact rakhne ki kosish karo, pointer mat bachao, extra lge toh lga lo
+
+
 class Solution {
     
     public boolean isPalindrome(ListNode head) {
-        if(head == null || head.next == null) return true;
+        if(head == null || head.next == null) return true; // Aadat
         
-        ListNode tempHead1 = head;
+        ListNode mid = middleNode(head);             
         
-        ListNode middleNode = middleNode(head);            // tempHead2 is the last node of the original list 
-        ListNode tempHead2 = reverseList(middleNode.next); // 1st middle k next wale element se reverse karna shuru karo
+        // 1st middle k next wale element se reverse karna shuru karo
+        // head2 is the last node of the original list 
+        ListNode head2 = reverseList(mid.next);     // 2nd half LL is reversed
+        
+        mid.next = null;    // 1st LL k tail(mid) ko null kar do, Ab 2 alag alag LL bn chuke hai
         
         // Agar odd length ki list huyi toh reverse wali list choti hogi ek se, islye rev(tempHead2) ko null se check karwao
         
         boolean isPali = true;
-        while(tempHead2 != null) {
+        ListNode tempHead1 = head;
+        ListNode tempHead2 = head2;
+        // Iterate from start to end of both LL to check Palindrome
+        while(tempHead2 != null && tempHead1 != null) {     // tempHead1 k wazah se loop kvi break nai hogi
             if(tempHead1.val != tempHead2.val) {
                 isPali = false;
                 break;
@@ -21,8 +31,18 @@ class Solution {
             tempHead1 = tempHead1.next;
             tempHead2 = tempHead2.next;
         } 
+
+        // LL ko last me unmodify kar k return karna chahiye
+        // Jo reverse kiya tha usko unreverse karo
+        ListNode midNext = reverseList(head2);  // midNext is the original LL's midNext
+        mid.next = midNext;                     // Original mid k next me midNext ko daal do
         
-        // LL ko last me unmodify kar k return karna chahiye, but not implemented here    
+        // Checking LL is unmodified 
+        // while(head != null) {
+        //     System.out.println(head.val);
+        //     head = head.next;
+        // }
+        
         return isPali;
     }
     
@@ -43,7 +63,7 @@ class Solution {
     
     
     public ListNode reverseList(ListNode head) {
-        if(head == null || head.next == null) return head;  // Aadat
+        if(head == null || head.next == null) return head;  
         
         ListNode prev = null;
         ListNode curr = head;
