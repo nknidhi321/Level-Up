e4f554ecde0ec03e3306327eb2c35bc56f0ea771
Nidhi Kumari <nknidhi321@gmail.com>
2022-03-05 14:18:46 +0530

    Update _2.1.4_number-of-enclaves.java

--- graph/_2.1.4_number-of-enclaves.java
+++ graph/_2.1.4_number-of-enclaves.java
@@ -17,6 +17,7 @@ class Solution {
         int m = grid[0].length;
         int[][] dir = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
         
+        // Making dfs call on all 1's boundary, and marking it as -1
         for(int i = 0; i < n; i ++) {
             for(int j = 0; j < m; j++) {
                 if(i == 0 || i == n - 1 || j == 0 || j == m - 1) {
@@ -27,19 +28,22 @@ class Solution {
             }
         }
         
+        
+        // Now, Count all 1's (which is bich wale 1's)
+        // Also mark the -1 cells as 1, just for not modifing the original grid
         int count = 0;
         for(int i = 0; i < n; i ++) {
             for(int j = 0; j < m; j++) {
-                if(grid[i][j] == 1) {
-                    count++;  
-                }
+                if(grid[i][j] == 1) count++;  // Counting 1's
+                else if(grid[i][j] == -1) grid[i][j] = 1; // Make the grid as how it was originally
             }
         }
         return count;
     }
     
+     
      public static void dfs(int i, int j , int[][] dir, int[][] grid) {
-        grid[i][j] = 0;
+        grid[i][j] = -1;
         
         for(int d = 0; d < dir.length; d++) {
             int x = i + dir[d][0];
