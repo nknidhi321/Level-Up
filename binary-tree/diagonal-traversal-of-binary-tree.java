3819c63a1e60dab670442dd0b0a56de4b65cb8cb
Nidhi Kumari <nknidhi321@gmail.com>
2022-03-16 14:39:00 +0530

    Create diagonal-traversal-of-binary-tree.java

---
+++ binary-tree/diagonal-traversal-of-binary-tree.java
@@ -0,0 +1,28 @@
+// https://practice.geeksforgeeks.org/problems/diagonal-traversal-of-binary-tree/1/#
+// For your answer keep on travelling to the right, and for the next level diagonal traversal add only your left node in the queue
+
+// O(2N) Every node will be visited twice once while adding once while removing
+
+class Tree {
+    
+     public ArrayList<Integer> diagonal(Node root) {
+         if(root == null) return new ArrayList<>();
+         
+         ArrayList<Integer> ans = new ArrayList<>();
+         LinkedList<Node> queue = new LinkedList<>();
+         queue.add(root);
+         
+         while(!queue.isEmpty()) {
+             int size = queue.size();
+             while(size-- > 0) { // Travel all component of that diagonal level
+                 Node node = queue.removeFirst();
+                 while(node != null) { // Travelling on individual component  // Dry run on the given example TC
+                     if(node.left != null) queue.addLast(node.left);  // Add only left node in the queue, for the next diagonal level
+                     ans.add(node.data);  // Forming curr diagonal level answer
+                     node = node.right;  // Move to right to travel all nodes of that diagonal
+                 }
+             }
+         }
+         return ans;
+      }
+}
