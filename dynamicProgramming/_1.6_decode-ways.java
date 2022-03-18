b77166ee94c391324de72ae69c93437cdd2bdf56
Nidhi Kumari <nknidhi321@gmail.com>
2022-03-19 02:17:04 +0530

    Update _1.6_decode-ways.java

--- dynamicProgramming/_1.6_decode-ways.java
+++ dynamicProgramming/_1.6_decode-ways.java
@@ -88,4 +88,40 @@ class Solution {
         return dp[IDX];
     }
 }
+
+//XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+
+// Optimization
+
+class Solution {
+    
+    public int numDecodings(String digits) {
+        Integer[] dp = new Integer[digits.length() + 1];
+        return numDecodings_opti(digits);
+    }
+    
+    public static int numDecodings_opti(String s) {
+        int a = 1, b = 0;  // Imagine when there only 1 digit, then a = 1 & b = 0
+        for (int idx = s.length() - 1; idx >= 0; idx--) {
+            char ch = s.charAt(idx);
+            int sum = 0;
+            if (ch != '0') {
+                sum += a;
+
+                if (idx < s.length() - 1) {
+                    char ch1 = s.charAt(idx + 1);
+                    int num = (ch - '0') * 10 + (ch1 - '0');
+                    if (num <= 26)
+                        sum += b;
+                }
+            }
+
+            b = a;
+            a = sum;
+        }
+
+        return a;  // Note : and will be in a, not b
+    }
+}
+
 //XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
