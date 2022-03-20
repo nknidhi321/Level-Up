10a52076890f904eb8a0a1e6055651a6ff810a4a
Nidhi Kumari <nknidhi321@gmail.com>
2022-03-20 17:15:44 +0530

    Update _2.3_distinct-subsequences.java

--- dynamicProgramming/_2.3_distinct-subsequences.java
+++ dynamicProgramming/_2.3_distinct-subsequences.java
@@ -13,23 +13,27 @@ class Solution {
     
     public static int numDistinctMemo(int len1, int len2, String s1, String s2, Integer[][] dp) {
         if(len2 == 0) return dp[len1][len2] = 1; // Jis din target string s2 exhaust ho gayi, hame ek tareeka mil gaya
-        
-        // Jis din given string s (here s1) target t (here s2) se choti ho jaaegi,
-        // us din hm kvi target ko achieve he nahi kar paaenge from given s string
-        if(len1 < len2) return dp[len1][len2] = 0; 
+        if(len1 < len2) return dp[len1][len2] = 0; // When target/pattern string becomes smaller than s1
         
         if(dp[len1][len2] != null) return dp[len1][len2];
-        
-        int count = 0;
+            
         if(s1.charAt(len1 - 1) == s2.charAt(len2 - 1)) { // Dono string ki last letter same hai
-           count += numDistinctMemo(len1 - 1, len2 - 1, s1, s2, dp); // Including both the letters
+            int count = 0;
+            
+            // Including both the letters
+            count += numDistinctMemo(len1 - 1, len2 - 1, s1, s2, dp);
+            
+            // Agar dono string ki last letter same hai v, toh v agar given string s(here s1) se last letter to skip kare and 
+            // pass len1 - 1, tvi possibility hai ki may be aur koi substring mil jaaye jo match ho rahi ho us len2 se
+            count += numDistinctMemo(len1 - 1, len2, s1, s2, dp);
+            
+            return dp[len1][len2] = count;
         }
-        
-        // Agar dono string ki last letter same hai v, toh v agar given string s(here s1) se last letter to skip kare and 
-        // pass len1 - 1, tvi possibility hai ki may be aur koi substring mil jaaye jo match ho rahi ho us len2 se.
-        count += numDistinctMemo(len1 - 1, len2, s1, s2, dp); // Not include // s(s1) k nahi aane ki choice har baar hogi
-        return dp[len1][len2] = count;
+        else { // Both chars are different // Usi char ko aage dhoondho
+            return dp[len1][len2] = numDistinctMemo(len1 - 1, len2, s1, s2, dp); 
+        }
     }
+    
 }
 
 //XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
@@ -54,25 +58,32 @@ class Solution {
                     continue;
                 }
                 
-                // Jis din given string s (here s1) target t (here s2) se choti ho jaaegi,
-                // us din hm kvi target ko achieve he nahi kar paaenge from given s string
+                // When target/pattern string becomes smaller than s1
                 if(len1 < len2) {
                     dp[len1][len2] = 0;
                     continue;
                 }
 
-                int count = 0;
                 if(s1.charAt(len1 - 1) == s2.charAt(len2 - 1)) { // Dono string ki last letter same hai
-                   count += dp[len1 - 1][len2 - 1];  // numDistinctMemo(len1 - 1, len2 - 1, s1, s2, dp); // Including both the letters
-                }
+                    int count = 0;
 
-                // Agar dono string ki last letter same hai v, toh v agar given string s(here s1) se last letter to skip kare and 
-                // pass len1 - 1, tvi possibility hai ki may be aur koi substring mil jaaye jo match ho rahi ho us len2 se.
-                count += dp[len1 - 1][len2];  // numDistinctMemo(len1 - 1, len2, s1, s2, dp); // Not include // s(s1) k nahi aane ki choice har baar hogi
-                dp[len1][len2] = count;
+                    // Including both the letters
+                    count += dp[len1 - 1][len2 - 1]; // numDistinctMemo(len1 - 1, len2 - 1, s1, s2, dp);
+
+                    // Agar dono string ki last letter same hai v, toh v agar given string s(here s1) se last letter to skip kare and 
+                    // pass len1 - 1, tvi possibility hai ki may be aur koi substring mil jaaye jo match ho rahi ho us len2 se
+                    count += dp[len1 - 1][len2]; // numDistinctMemo(len1 - 1, len2, s1, s2, dp);
+
+                    dp[len1][len2] = count;
+                }
+                else { // Both chars are different // Usi char ko aage dhoondho
+                    dp[len1][len2] =  dp[len1 - 1][len2]; // numDistinctMemo(len1 - 1, len2, s1, s2, dp); 
+                }
             }
         }
+        
         return dp[LEN1][LEN2];
     }
 }
+
 //XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
