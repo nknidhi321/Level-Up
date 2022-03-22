f568024e1e5446f4b8616162f3eba0ec235dc258
Nidhi Kumari <nknidhi321@gmail.com>
2022-03-23 03:05:55 +0530

    Update _4.5_bounded-&-unbounded-01-knapsack.java

--- dynamicProgramming/_4.5_bounded-&-unbounded-01-knapsack.java
+++ dynamicProgramming/_4.5_bounded-&-unbounded-01-knapsack.java
@@ -54,12 +54,45 @@ class Solution {
     }
 }
 
+
+//---------------------------------------------------------------------------------------
+
+// Wrong
+// P&C, for loop
+// Using Combination here, passing n - 1, each time for unique items
+// Memo
+
+class Solution { 
+
+    static int knapSack(int w, int wt[], int val[], int n)  { 
+        Integer[][] dp = new Integer[n + 1][w + 1];
+        return knapSack_Memo(n, w, wt, val, dp);
+    } 
+    
+    public static int knapSack_Memo(int n, int w, int[] wt, int[] val, Integer[][] dp) {
+        if(n == 0 || w == 0) return dp[n][w] = 0;
+        
+        if(dp[n][w] != null) return dp[n][w];
+        
+        int max = 0;
+        for(int i = n; i > 0; i--) {
+            if(w - wt[i - 1] >= 0) {
+                max = Math.max(max, knapSack_Memo(n - 1, w - wt[i - 1], wt, val, dp) + val[i - 1]);
+            }
+        }
+        return dp[n][w] = max;
+    }
+    
+}
+
+
 //XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 
 // https://practice.geeksforgeeks.org/problems/knapsack-with-duplicate-items4201/1
 // Unbounded Knapsack [Same item can be used again, infine supply of item]
 
 // Memoization
+// Subsequence
 
 class Solution{
  
@@ -88,6 +121,7 @@ class Solution{
 // ------------------------------------------------------------------------------------------------------------
 
 // Tabulation
+// Subsequence
 
 class Solution{
   
@@ -113,6 +147,72 @@ class Solution{
         return dp[dp.length - 1][dp[0].length - 1];
     }
 
+}
+
+//---------------------------------------------------------------------------------------------------------------------------
+
+// P&C, for loop
+
+/*	
+	Here question is unbounded knapsack, so you can use permutation, so 1D DP is sufficient here
+	But if you would have gone for combination then:-
+	 1) You would also need to pass idx apart from target/w , so in that case you will need 2D DP
+	 2) Here its "unbounded", so your combination should pass n to the next iteration and not n - 1, to use same item again
+*/
+
+// Memoization
+
+class Solution {
+    
+    static int knapSack(int N, int w, int val[], int wt[]) {
+        Integer[] dp = new Integer[w + 1];
+        return knapSack_Memo(w, wt, val, dp);
+    } 
+    
+    public static int knapSack_Memo(int w, int[] wt, int[] val, Integer[] dp) {
+        if(w == 0) return dp[w] = 0;
+        
+        if(dp[w] != null) return dp[w];
+        
+        int max = 0;
+        for(int i = 0; i < wt.length; i++) {
+            if(w - wt[i] >= 0) {
+                max = Math.max(max, knapSack_Memo(w - wt[i], wt, val, dp) + val[i]);
+            }
+        }
+        return dp[w] = max;
+    }
+}
+
+//-------------------------------------------------------------------------------------------------------------
+
+// P&C, for loop
+// Tabulation
+
+class Solution {
+    
+    static int knapSack(int N, int w, int val[], int wt[]) {
+        Integer[] dp = new Integer[w + 1];
+        return knapSack_Tab(w, wt, val, dp);
+    } 
+    
+    public static int knapSack_Tab(int W, int[] wt, int[] val, Integer[] dp) {
+        for(int w = 0; w <= W; w++) {
+            if(w == 0) {
+                dp[w] = 0;
+                continue;
+            }
+            
+            int max = 0;
+            for(int i = 0; i < wt.length; i++) {
+                if(w - wt[i] >= 0) {
+                    max = Math.max(max, dp[w - wt[i]] + val[i]); //knapSack_Memo(w - wt[i], wt, val, dp) + val[i]);
+                }
+            }
+            dp[w] = max;
+        }
+        return dp[W];
+    }
 }
 
 //XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
