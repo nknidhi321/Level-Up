2e2daa4ceb11ecddc3571caa396781e85e36c9e1
Nidhi Kumari <nknidhi321@gmail.com>
2022-03-22 12:47:22 +0530

    Update _4.1_coin-change-permutations.java

--- dynamicProgramming/_4.1_coin-change-permutations.java
+++ dynamicProgramming/_4.1_coin-change-permutations.java
@@ -1,5 +1,7 @@
 // https://www.pepcoding.com/resources/online-java-foundation/dynamic-programming-and-greedy/coin-change-permutations-official/ojquestion
 
+// Memoization
+
 import java.io.*;
 import java.util.*;
 
@@ -35,3 +37,50 @@ public class Main {
         return dp[tar] = count;
     }
 }
+
+
+//XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+
+// Tabulation
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int n  = sc.nextInt();
+        int[] coins = new int[n];
+        for(int i = 0; i < n; i++) {
+            coins[i] = sc.nextInt();
+        }
+        int tar = sc.nextInt();
+        System.out.println(change(tar, coins));
+    }
+    
+    public static int change(int tar, int[] coins) {
+        int[] dp = new int[tar + 1]; 
+        return change_Tab(tar, coins, dp);
+    }
+    
+    public static int change_Tab(int Tar, int[] coins, int[] dp) {
+        for(int tar = 0; tar <= Tar; tar++) {
+            if(tar == 0) {
+                dp[tar] = 1;
+                continue;
+            }
+            
+            int count = 0;
+            for(int ele : coins) {
+                if(tar - ele >= 0) {
+                    count += dp[tar - ele]; // change_Memo(tar - ele, coins, dp);
+                } 
+            }
+            dp[tar] = count;
+        }
+        return dp[Tar];
+    }
+}
+
+//XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
