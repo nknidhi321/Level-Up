537aa8c5e4bcafe0656f8683e71f3612ec01a80b
Nidhi Kumari <nknidhi321@gmail.com>
2022-03-22 21:43:20 +0530

    Update _4.3_subset-sum-problem.java

--- dynamicProgramming/_4.3_subset-sum-problem.java
+++ dynamicProgramming/_4.3_subset-sum-problem.java
@@ -1,6 +1,8 @@
 // https://practice.geeksforgeeks.org/problems/subset-sum-problem-1611555638/1/ 
 // If infinite supply of coins is not mentioned then, consider it as, coins can be used only once
+// Not using boolean DP because false is also ananswer, Boolean DP could have been used.
 
+// Subsequence 
 // Memoization
 
 class Solution {
@@ -27,31 +29,43 @@ class Solution {
     
 }
 
-//XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+//----------------------------------------------------------------------------------------------
 
 // Tabulation
 
+
+//XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+
+// P&C method, for loop
+// Memoization
+
 class Solution {
 
-    public static Boolean isSubsetSum(int n, int arr[], int tar) {
-        
-        Integer[][] dp = new Integer[n + 1][tar + 1];
-        return isSubsetSum_Memo(n, arr, tar, dp) == 1 ? true : false;
+    static Boolean isSubsetSum(int N, int[] arr, int tar) {
+        Integer[][] dp = new Integer[N + 1][tar + 1];
+        return isSubsetSum_Memo(0, arr, tar, dp) == 1 ? true : false;
     }
     
-    public static int isSubsetSum_Memo(int n, int[] arr, int tar, Integer[][] dp) {
-        if(n == 0 || tar == 0) return dp[n][tar] = tar == 0 ? 1 : 0; 
+    public static int isSubsetSum_Memo(int idx, int[] arr, int tar, Integer[][] dp) {
+        if(tar == 0 || idx == arr.length) return dp[idx][tar] = (tar == 0) ? 1 : 0;
         
-        if(dp[n][tar] != null) return dp[n][tar];
+        if(dp[idx][tar] != null) return dp[idx][tar];
         
-        boolean res = false;
-        if(tar - arr[n - 1] >= 0) {
-            res = res || isSubsetSum_Memo(n - 1, arr, tar - arr[n - 1], dp) == 1; // Include
+        for(int i = idx; i < arr.length; i++) {
+            if(tar - arr[i] >= 0) {
+                if(isSubsetSum_Memo(i + 1, arr, tar - arr[i], dp) == 1) {
+                    return dp[idx][tar] = 1;
+                }
+            }
         }
-        res = res || isSubsetSum_Memo(n - 1, arr, tar, dp) == 1; // Exclude
-        return dp[n][tar] = res ? 1 : 0;
+        return dp[idx][tar] = 0;
     }
     
 }
 
-//XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+//-----------------------------------------------------------------------------------------------
+
+
+
+
+//XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
