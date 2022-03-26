b2e131f68bb09968d9cd8bbc068bee3707a4984c
Nidhi Kumari <nknidhi321@gmail.com>
2022-03-27 02:57:42 +0530

    Update _5.1_left-view-of-binary-tree.java

--- binary-tree/_5.1_left-view-of-binary-tree.java
+++ binary-tree/_5.1_left-view-of-binary-tree.java
@@ -24,3 +24,34 @@ class Tree {
         return ans;
     }
 }
+
+//XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+
+// Follow up :-
+// https://leetcode.com/problems/find-bottom-left-tree-value/
+// Last level ki sbse pehli node is your answer
+
+class Solution {
+    
+    public int findBottomLeftValue(TreeNode root) {
+    
+        LinkedList<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        
+        int leftMostValue = -1;
+        while(!queue.isEmpty()) {
+            int size = queue.size();
+            leftMostValue = queue.getFirst().val;
+            
+            while(size-- > 0) {
+                TreeNode curr = queue.removeFirst();
+                if(curr.left != null) queue.add(curr.left);
+                if(curr.right != null) queue.add(curr.right);
+            }
+        }
+        return leftMostValue;
+    }
+
+}
+
+//XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
