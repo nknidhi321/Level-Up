6d3c6e846e2b9ff8d3c2dc00b19545d568875981
Nidhi Kumari <nknidhi321@gmail.com>
2022-03-27 02:13:05 +0530

    Update _5.8_diagonal-traversal-of-binary-tree.java

--- binary-tree/_5.8_diagonal-traversal-of-binary-tree.java
+++ binary-tree/_5.8_diagonal-traversal-of-binary-tree.java
@@ -29,3 +29,90 @@ class Tree {
          return ans;
       }
 }
+
+//XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+// https://www.pepcoding.com/resources/data-structures-and-algorithms-in-java-levelup/trees/diagonal-order-of-a-binarytree/ojquestion
+// Same question as above just expecting answer in separate diagonal level in list 
+
+import java.util.*;
+
+public class Main {
+    public static Scanner scn = new Scanner(System.in);
+
+    public static class TreeNode {
+        int val = 0;
+        TreeNode left = null;
+        TreeNode right = null;
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+    }
+
+
+    //=======================================================================================================================================
+    public static ArrayList<ArrayList<Integer>> diagonalOrder(TreeNode root) {
+         if(root == null) return new ArrayList<>();
+         
+         ArrayList<ArrayList<Integer>> ans = new ArrayList<>();
+         LinkedList<TreeNode> queue = new LinkedList<>();
+         queue.add(root);
+         
+         int level = 0;
+         while(!queue.isEmpty()) {
+             int size = queue.size();
+             while(size-- > 0) { // Travel all component of that diagonal level
+                 TreeNode node = queue.removeFirst();
+                 while(node != null) { // Travelling on individual component  // Dry run on the given example TC
+                     if(node.left != null) queue.addLast(node.left);  // Add only left node in the queue, for the next diagonal level
+                     if(ans.size() == level) ans.add(new ArrayList<>());
+                     
+                     ans.get(level).add(node.val);  // Forming curr diagonal level answer
+                     node = node.right;  // Move to right to travel all nodes of that diagonal
+                 }
+             }
+             level++;
+         }
+         return ans;
+    }
+    //===========================================================================================================================================
+
+
+
+    public static TreeNode createTree(int[] arr, int[] IDX) {
+        if (IDX[0] > arr.length || arr[IDX[0]] == -1) {
+            IDX[0]++;
+            return null;
+        }
+        TreeNode node = new TreeNode(arr[IDX[0]++]);
+        node.left = createTree(arr, IDX);
+        node.right = createTree(arr, IDX);
+
+        return node;
+    }
+
+    public static void solve() {
+        int n = scn.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++)
+            arr[i] = scn.nextInt();
+
+        int[] IDX = new int[1];
+        TreeNode root = createTree(arr, IDX);
+
+        ArrayList<ArrayList<Integer>> ans = diagonalOrder(root);
+        int idx = 0;
+        for (ArrayList<Integer> i : ans) {
+            System.out.print(idx++ + " -> ");
+            for (Integer j : i)
+                System.out.print(j + " ");
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        solve();
+    }
+}
+
+//XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
