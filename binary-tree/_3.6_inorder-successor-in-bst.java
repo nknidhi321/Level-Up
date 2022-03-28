453b2f26dd793fb5be428368b19caf4174948b63
Nidhi Kumari <nknidhi321@gmail.com>
2022-03-28 20:21:55 +0530

    Update _3.6_inorder-successor-in-bst.java

--- binary-tree/_3.6_inorder-successor-in-bst.java
+++ binary-tree/_3.6_inorder-successor-in-bst.java
@@ -1,27 +1,25 @@
 // https://practice.geeksforgeeks.org/problems/inorder-successor-in-bst/1/
 
 class Solution {
-     
-	public Node inorderSuccessor(Node root, Node k) {
-	    Node returnValue = null;
-	    int prev = -1; 
-	    Node curr = root;
-        while(curr != null) {
+
+    public Node inorderSuccessor(Node root, Node k) {
+        Node returnValue = null;
+        int prev = -1;
+        Node curr = root;
+        while (curr != null) {
             Node left = curr.left;
-            if(left == null) {
-                if(prev == k.data) returnValue = curr;
+            if (left == null) {
+                if (prev == k.data) returnValue = curr;
                 prev = curr.data;
                 curr = curr.right;
-            }
-            else {
+            } else {
                 Node rightMostNode = getRightMostNode(left, curr);
-                if(rightMostNode.right == null) { // thread create
+                if (rightMostNode.right == null) { // thread create
                     rightMostNode.right = curr;
                     curr = curr.left;
-                }
-                else { // thread destroy
+                } else { // thread destroy
                     rightMostNode.right = null;
-                    if(prev == k.data) returnValue = curr;
+                    if (prev == k.data) returnValue = curr;
                     prev = curr.data;
                     curr = curr.right;
                 }
@@ -29,12 +27,12 @@ class Solution {
         }
         return returnValue;
     }
-  
-    public static Node getRightMostNode(Node node, Node curr) {	
-        while (node.right != null && node.right != curr) { 
-          node = node.right;
+
+    public static Node getRightMostNode(Node node, Node curr) {
+        while (node.right != null && node.right != curr) {
+            node = node.right;
         }
         return node;
     }
-   
+
 }
