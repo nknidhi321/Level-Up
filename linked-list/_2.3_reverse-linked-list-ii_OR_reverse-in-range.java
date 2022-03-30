d373c9663317d5cc4c26cae6855a89d5f63688cb
Nidhi Kumari <nknidhi321@gmail.com>
2022-03-31 01:39:33 +0530

    Update _2.3_reverse-linked-list-ii_OR_reverse-in-range.java

--- linked-list/_2.3_reverse-linked-list-ii_OR_reverse-in-range.java
+++ linked-list/_2.3_reverse-linked-list-ii_OR_reverse-in-range.java
@@ -55,3 +55,62 @@ class Solution {
     }
     
 }
+//------------------------------------------------------------------
+
+// same as above just When left is 1, Handling the 1st node by dummy
+
+class Solution {
+    
+    // Make sure to make th & tt global else addFirstInTempList will have to return both th and tt, since it is call by value
+    // So when addFirstInTempList function returns th and tt in main will still be pointing to where it was poiting.   
+    public ListNode th = null;  // th = tempHead
+    public ListNode tt = null;  // tt = tempTail
+    
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+        if(head == null || head.next == null) return head;
+        
+        ListNode curr = head;
+        
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+        ListNode prev = dummy;
+        int idx = 1;
+        
+        while(idx <= right) { 
+            if(idx == left - 1) prev = curr; // Stand just before left
+            
+            if(idx >= left && idx <= right) { // Do addFirst for [left, right] nodes to reverse in tempList 
+                while(idx >= left && idx <= right) {
+                    ListNode forward = curr.next; // Backup
+                    curr.next = null; // Break link with next node
+                    addFirstInTempList(curr); // By addFirst we are reversing node in tempList
+                    curr = forward; // for next iteration
+                    idx++;
+                }
+            }
+            else {
+                curr = curr.next;  // Move to next node until you reach to left
+                idx++;
+            }
+        }
+        
+        prev.next = th; // else add remaining left nodes to the th 
+        tt.next = curr;  // Add remaining right nodes to the tt
+        
+        return dummy.next;
+    }
+    
+    
+    // Creating a tempList and doing addFirst
+    public void addFirstInTempList(ListNode node) {
+        if(th == null) {
+            th = node;
+            tt = node;
+        }
+        else {
+            node.next = th;
+            th = node;
+        }
+    }
+    
+}
