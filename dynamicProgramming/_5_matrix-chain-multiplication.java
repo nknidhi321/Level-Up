0c448e617cfe051647a5d99679ff4fa99fab2400
Nidhi Kumari <nknidhi321@gmail.com>
2022-04-06 12:32:18 +0530

    Update _5_matrix-chain-multiplication.java

--- dynamicProgramming/_5_matrix-chain-multiplication.java
+++ dynamicProgramming/_5_matrix-chain-multiplication.java
@@ -1,4 +1,6 @@
 // https://practice.geeksforgeeks.org/problems/matrix-chain-multiplication0303/1 
+// Do checkout the follow up
+
 // si is increasing and ei is decreasing so obviously apply gap strategy
 
 class Solution{
@@ -69,3 +71,51 @@ class Solution{
 }
 
 //XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+
+// Follow up :- 
+// Print the minimum cost matrix example like ((A(BC))D)
+
+    public static int mcm_Dp(int[] arr, int SI, int EI, int[][] dp) {
+        int n = arr.length;
+        String[][] sdp = new String[n][n];
+
+        for (int gap = 1; gap < n; gap++) {
+            for (int si = 0, ei = gap; ei < n; si++, ei++) {
+                if (ei - si == 1) {
+                    dp[si][ei] = 0;
+                    sdp[si][ei] = (char) (si + 'A') + "";
+                    continue;
+                }
+
+                int minRes = (int) 1e9;
+                String minStr = "";
+                for (int cut = si + 1; cut < ei; cut++) {
+                    int leftRes = dp[si][cut];// mcm_memo(arr, si, cut, dp);
+                    int rightRes = dp[cut][ei];// mcm_memo(arr, cut, ei, dp);
+
+                    if (leftRes + arr[si] * arr[cut] * arr[ei] + rightRes < minRes) {
+                        minRes = leftRes + arr[si] * arr[cut] * arr[ei] + rightRes;
+                        minStr = "(" + sdp[si][cut] + sdp[cut][ei] + ")";
+                    }
+                }
+
+                dp[si][ei] = minRes;
+                sdp[si][ei] = minStr;
+            }
+
+        }
+        System.out.println(sdp[SI][EI]);
+        return dp[SI][EI];
+    }
+
+    public static void mcm() {
+        int[] arr = { 10, 30, 5, 60 };
+        int n = arr.length;
+        int[][] dp = new int[n][n];
+
+        System.out.println(mcm_Dp(arr, 0, n - 1, dp));
+
+    }
+
+
+//XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
