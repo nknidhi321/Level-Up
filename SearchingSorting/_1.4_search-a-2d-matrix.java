48bf1bd531535cb9766008a42f4801b4361804c7
Nidhi Kumari <nknidhi321@gmail.com>
2022-04-07 14:27:55 +0530

    Update and rename search-a-2d-matrix.java to _1.4_search-a-2d-matrix.java

--- SearchingSorting/_1.4_search-a-2d-matrix.java
+++ SearchingSorting/_1.4_search-a-2d-matrix.java
@@ -27,24 +27,26 @@ class Solution {
 }
 ```
 
-----------------------------------------------------------------------------------
+// NOTE : It is not specified ki tumse niche wale tumse bade hai, but since it is sortes so tumse niche wale tumse bade he honge for sure
+// So this can be also solved like below follow up
+-------------------------------------------------------------------------------------------------------------------------------------------
 
-// Archit
+// Follow up :- https://leetcode.com/problems/search-a-2d-matrix-ii/
+
+// Rajneesh
 // Staircase Search -> O(N + M) in Worst Case
+// Note : Here you cannot apply original Binary Search like above because though it array is sorted but not how it is expected in BS, so apply modified BS
 
 ```
 class Solution {
     
     public boolean searchMatrix(int[][] matrix, int target) {
-        int n = matrix.length, m = matrix[0].length; 
+           int n = matrix.length, m = matrix[0].length; 
         
         // Tumse niche wale sab tumse bade hai, tumse left wale sab tumse chote hai
         // So simply apply binary search
         
-        // We will start searching from top right corner, if found well & good else
-        // Chote elements k liye left jaao
-        // Bade elements k liye niche jaao
-        
+        // We will start searching from top right corner
         int row = 0; 
         int col = m - 1;
         
@@ -59,4 +61,4 @@ class Solution {
 }
 ```
 
---------------------------------------------------------------------------------------
+---------------------------------------------------------------------------------------------------------------------------------------------
