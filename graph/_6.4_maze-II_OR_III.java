c3df147e615ebd5b6267ceafb881f695caa6c041
Nidhi Kumari <nknidhi321@gmail.com>
2022-04-15 16:19:25 +0530

    ======================IMP+++++++++++++++

--- graph/_6.4_maze-II_OR_III.java
+++ graph/_6.4_maze-II_OR_III.java
@@ -1,4 +1,8 @@
+// Check the follow up at below
+
 // https://www.lintcode.com/problem/788/
+// Minimum shortest steps
+
 // Without dis[]
 
 public class Solution {
@@ -107,4 +111,81 @@ public class Solution {
     
 }
 
-//--------------------------------------------------------------------------------------------------------------------------------------------------------------
+//XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+
+// Follow up  // https://www.lintcode.com/problem/789/
+// Minimum shortest steps + lex shortest path 
+
+
+public class Solution {
+
+    public static class pair implements Comparable<pair> {
+        int r = 0, c = 0, steps = 0;
+        String psf = "";
+
+        pair(int r, int c, int steps, String psf) {
+            this.r = r;
+            this.c = c;
+            this.steps = steps;
+            this.psf = psf;
+        }
+
+        @Override
+        public int compareTo(pair o) {
+            if (this.steps != o.steps)
+                return this.steps - o.steps;
+            else
+                return this.psf.compareTo(o.psf);
+        }
+    }
+
+    public String findShortestWay(int[][] maze, int[] start, int[] destination) {
+        int n = maze.length, m = maze[0].length, sr = start[0], sc = start[1], er = destination[0], ec = destination[1];
+        int[][] dir = { { 1, 0 }, { -1, 0 }, { 0, 1 }, { 0, -1 } };
+        String[] dirS = { "d", "u", "r", "l" };
+        pair[][] dis = new pair[n][m];
+        for (int i = 0; i < n * m; i++)
+            dis[i / m][i % m] = new pair(i / m, i % m, (int) 1e8, "");
+
+        PriorityQueue<pair> que = new PriorityQueue<>();
+        pair src = new pair(sr, sc, 0, "");
+
+        que.add(src);
+        dis[sr][sc] = src;
+
+        while (que.size() != 0) {
+            pair p = que.remove();
+            for (int i = 0; i < 4; i++) {
+                int[] d = dir[i];
+
+                int r = p.r, c = p.c, steps = p.steps;
+                while (r >= 0 && c >= 0 && r < n && c < m && maze[r][c] == 0 && !(r == er && c == ec)) { // hole pe v tum break kar jaaoge
+                    r += d[0];
+                    c += d[1];
+                    steps++;
+                }
+
+                if (!(r == er && c == ec)) { // hole invalid cell nahi hai, islye hole pe -- nahi hoga
+                    r -= d[0];
+                    c -= d[1];
+                    steps--;
+                }
+
+                pair np = new pair(r, c, steps, p.psf + dirS[i]);
+                
+                // In first check, koi zyada steps aari hai toh continue kar jaao
+                // Equal steps pe second check lgega kuki lex choti path aa sakti hai 
+                // In second check, dis[r][c] pe already lex choti path hai, toh q update karwau continue kar jaao 
+                if (steps > dis[r][c].steps || dis[r][c].compareTo(np) <= 0) continue;
+
+                que.add(np);
+                dis[r][c] = np;
+            }
+        }
+
+        pair ans = dis[er][ec];
+        return ans.steps != (int) 1e8 ? ans.psf : "impossible";
+    }
+}
+
+//XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
