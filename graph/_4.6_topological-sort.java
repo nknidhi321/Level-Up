f18ad71f8f4dc2beec988864ecc3798e2a91440b
Nidhi Kumari <nknidhi321@gmail.com>
2022-04-22 19:20:48 +0530

    Update _4.6_topological-sort.java

--- graph/_4.6_topological-sort.java
+++ graph/_4.6_topological-sort.java
@@ -1,5 +1,49 @@
 // https://practice.geeksforgeeks.org/problems/topological-sort/1#
 
+// TopoDFS, 1st step of Kosaraju
+
+class Solution {
+
+    public static int[] topoSort(int V, ArrayList<ArrayList<Integer>> adj) {
+    
+       boolean[] vis = new boolean[V];
+                
+        // Step 1 : DFS (Random) of Kosaraju
+        Stack<Integer> stack = new Stack<>();
+        for(int i = 0; i < V; i++) {
+            if(!vis[i]) {
+                DFS_1(i, vis, adj, stack);
+            }
+        }
+
+        // Itertate from back, because u is at the top of the stack 
+        int i = 0; 
+        int[] ans = new int[V];
+        while(!stack.isEmpty()) {
+            ans[i++] = stack.pop();
+        }   
+        return ans;
+    }
+    
+    
+    public static void DFS_1(int src, boolean[] vis, ArrayList<ArrayList<Integer>> adj, Stack<Integer> stack) {
+        vis[src] = true;
+        
+        ArrayList<Integer> nbrs = adj.get(src);
+        for(int nbr : nbrs) {
+            if(!vis[nbr]) {
+                DFS_1(nbr, vis, adj, stack);
+            }
+        }
+        stack.push(src);
+    }
+    
+}
+
+//================================================================================================================================================================
+
+// Kahn's Algo, kind of BFS
+
 class Solution {
 
     public static int[] topoSort(int V, ArrayList<ArrayList<Integer>> adj) {
@@ -35,3 +79,6 @@ class Solution {
     }
     
 }
+
+
+//================================================================================================================================================================
