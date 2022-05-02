01fe78a26010a060a88403c4375750c9608b5792
Nidhi Kumari <nknidhi321@gmail.com>
2022-05-02 19:35:58 +0530

    Update _1.7_find-k-closest-elements.java

--- SearchingSorting/_1.7_find-k-closest-elements.java
+++ SearchingSorting/_1.7_find-k-closest-elements.java
@@ -3,12 +3,14 @@
     Saare elements ka diff nikal lo given x se, and is diff k basis pe maxHeap ka Comparable likh do.
 
     Note the below condition :
-    ------------------
+    -------------------------
     An integer a is closer to x than an integer b if
     |a - x| < |b - x|, or => Agar a ka diff chota hai, toh a closer hai
     |a - x| == |b - x| and a < b  =>  Agar a b dono ka diff same hai, then smaller element will be considered as closer element =>
     Smaller ele should stay in the maxHeap and larger ele should be popped out.
     So maxHeap se bade element ko nikalna hoga.
+    
+    TC : N(logK) for PQ addition removal + K(logK) for sorting
 */
 
 ```
