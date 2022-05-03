d3f6e515c3f22b0d0a24bfba8be9713da2f983f3
Nidhi Kumari <nknidhi321@gmail.com>
2022-05-03 02:25:06 -0700

    Update _1.7_find-k-closest-elements.java

--- SearchingSorting/_1.7_find-k-closest-elements.java
+++ SearchingSorting/_1.7_find-k-closest-elements.java
@@ -1,4 +1,67 @@
 // https://leetcode.com/problems/find-k-closest-elements/
+
+// Using lastInsertPos, BS
+// Rajneesh
+
+```
+class Solution {
+    
+    public List<Integer> findClosestElements(int[] arr, int k, int x) {
+        
+        int lastInsertPos = lastInsertPos(0, arr.length - 1, x, arr);        
+		// Now you know closest ele from x will be at lastInsertPos or lastInsertPos-1
+				
+        List<Integer> ans = new ArrayList<>();
+        int left = lastInsertPos - 1;
+        int right = lastInsertPos;
+
+        // Apna range left and right k taraf expand karo, and pick acc.
+        while(left >= 0 && right < arr.length && k-- > 0) {
+            if(x - arr[left] <= arr[right] - x) ans.add(arr[left--]);        
+            else ans.add(arr[right++]);
+        }
+        
+        // Right k wazah end hua tha upar ka loop && k baaki hai avi
+        while(left >= 0 && k-- > 0) ans.add(arr[left--]);
+        
+        // Left k wazah end hua tha upar ka loop && k baaki hai avi
+        while(right < arr.length && k-- > 0) ans.add(arr[right++]);
+        
+        Collections.sort(ans); // Answer is expected in sorted order
+        return ans;
+    }
+    
+    public int lastInsertPos(int si, int ei, int x, int[] arr) {
+        while(si <= ei) {
+            int mid = si + (ei - si) / 2;
+            // Last insert pos chahiye, so mil jaaye tb v right me jaao 
+            if(arr[mid] <= x) si = mid + 1; 
+            else ei = mid - 1;
+        }
+        return si; // si hamesha lastInsertPos pe point karega
+    }
+     
+}
+```
+----------------------------------------------------------------------
+// YT pepcoding Sol. is wrong
+// This will not give you the closestIdx
+// Ex : [1,5,10]  k = 1, x = 4   
+// Here closestIdx should be 1 i.e. 5, but we will get 0 i.e. 1
+public int binarySearchClosestIdx(int si, int ei, int x, int[] arr) {
+    int mid = -1;
+    while(si <= ei) {
+        mid = si + (ei - si) / 2;
+        if(arr[mid] == x) return mid;
+        else if(arr[mid] < x) si = mid + 1;
+        else ei = mid - 1;
+    }
+    return mid;
+}
+    
+//XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+
+// Using PQ
 /*
     Saare elements ka diff nikal lo given x se, and is diff k basis pe maxHeap ka Comparable likh do.
 
