3fa521a964583144344d994cecf9d5609275fd80
Nidhi Kumari <nknidhi321@gmail.com>
2022-05-09 13:36:58 +0530

    Update _1.9_painting-the-fence.java

--- dynamicProgramming/_1.9_painting-the-fence.java
+++ dynamicProgramming/_1.9_painting-the-fence.java
@@ -27,3 +27,33 @@ class Solution {
     }
 
 }
+
+//XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+// Optimization from the recurrence relation
+
+class Solution {
+    
+    public long countWays(int n, int k) {
+        if(n == 1) return k;
+        
+        long mod = (long)1e9 + 7;
+
+	// Last 2 colors Ending at ii(same color)
+	long same = k * 1; // repeation allowed
+	    
+	// Last 2 colors Ending at ij(diff color)
+	long diff = k * (k - 1); // repeation not allowed 
+		
+	long total = (same + diff) % mod;  // For n == 2
+		
+        for(int i = 3; i <= n; i++) {
+            same = diff * 1;
+            diff = (total * (k - 1)) % mod;
+            total = (same + diff) % mod;
+        }
+        return total;
+    }
+
+}
+
+//XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
