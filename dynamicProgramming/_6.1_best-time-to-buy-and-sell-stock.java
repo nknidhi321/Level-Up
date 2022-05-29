af22570dc07ddc6811026e196594e19c7ee47d5e
Nidhi Kumari <nknidhi321@gmail.com>
2022-05-29 01:53:27 -0700

    Update _6.1_best-time-to-buy-and-sell-stock.java

--- dynamicProgramming/_6.1_best-time-to-buy-and-sell-stock.java
+++ dynamicProgramming/_6.1_best-time-to-buy-and-sell-stock.java
@@ -35,4 +35,47 @@ class Solution {
     
 }
 ```
--------------------------------------------------------------
+
+---------------------------------------------------------------------------------------------------------------------------
+
+// Buy Sell Stock formula solution 
+// Kind of include exclude 
+// Bechoge tvi jab ek stock buy kara hua hai
+// NOTE: Same day pe buy sell karne ka sense nai bnta hai, kuki 0 profit hogi
+
+```
+class Solution {
+    
+    public int maxProfit(int[] prices) {
+        int n = prices.length;
+        
+        // Initially 0 rupee hai wallet me, toh kuch kharioge toh -ve me he jaaoge
+        int obsp = -prices[0]; // old Bought State Profit
+        
+        // Curent state se pehle na kuch kharida, na kuch becha, so sell state ki profit will be 0 
+        int ossp = 0; // old Sold State Profit
+        
+        for(int i = 1; i < n; i++) {
+            
+            // will you buy on the current day ?? // Buy karoge toh wallet k paise ghatenge 
+            // Agar Buy krte ho toh pichle state k sell pe "nahi" karoge // Buy buy is not allowed
+            // Ya toh apna pichla state hold kr lo ya "new" buy kr lo
+            int nbsp = Math.max(obsp, 0 - prices[i]); 
+            
+            // will you sell on the current day ?? // Sell karoge toh wallet k paise badhenge
+            // Agar sell krte ho toh pichle state k buy pe karoge // Kuki Sell Sell is not allowed
+            // Ya toh apna pichla state hold kr lo ya sell kr lo 
+            // NOTE : Yaha pe "abtak jitne paise kamaye uspe" sell kr lo is not included
+            // kuki obsp hamare pichle x transaction ko include karte huye "nahi" bna hai  [0 - prices[i]]
+            int nssp = Math.max(ossp, obsp + prices[i]); 
+            
+            obsp = nbsp;
+            ossp = nssp;
+        }
+        
+        return ossp; // Max hamesha sold state pe he milega, kuki buy krte waqt -price[i] krte hai
+    }
+    
+}
+```
+-----------------------------------------------------------------------------------------------------------------------------------
