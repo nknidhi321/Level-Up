1c35948cadc8128e3f0b8cfaaa33d6f3b836903f
Nidhi Kumari <nknidhi321@gmail.com>
2022-06-12 14:29:09 +0530

    Update _1.5_inversion-of-array.java

--- SearchingSorting/_1.5_inversion-of-array.java
+++ SearchingSorting/_1.5_inversion-of-array.java
@@ -32,58 +32,47 @@ class Solution {
 
 class Solution {
 
-    public static long count;
-    
     public static long inversionCount(long arr[], long n) {
-        count = 0;
-        mergeSort(0, n - 1, arr);
-        return count;
+        long[] count = new long[1];
+        mergeSort(0, n - 1, arr, count);
+        return count[0];
     }
 
-    public static long[] mergeSort(long lo, long hi, long[] arr) {
-        if (lo > hi) return new long[0];
+    public static long[] mergeSort(long lo, long hi, long[] arr, long[] count) {
         if (lo == hi) return new long[] {arr[(int)lo]}; // Creating new array and adding that one element
 
         long mid = lo + (hi - lo) / 2;
-        long[] a = mergeSort(lo, mid, arr);
-        long[] b = mergeSort(mid + 1, hi, arr);
+        long[] a = mergeSort(lo, mid, arr, count);
+        long[] b = mergeSort(mid + 1, hi, arr, count);
         
-        return mergeTwoSortedArrays(a, b);
+        return mergeTwoSortedArrays(a, b, count);
     }
 
     //used for merging two sorted arrays
-    public static long[] mergeTwoSortedArrays(long[] a, long[] b) {
+    public static long[] mergeTwoSortedArrays(long[] a, long[] b, long[] count) {
         int i = 0, j = 0, k = 0;
         long[] ans = new long[a.length + b.length];
+
         while (i < a.length && j < b.length) {
             if (a[i] <= b[j]) { // ith idx wala bnda chota hai
-                ans[k] = a[i];
-                i++;
-                k++;
+                ans[k++] = a[i++];
             } else { // jth idx wala bnda chota hai
-                // mtlb jth idx wala bnda, ith idx se, ith idx k end tak sab se inversion count bnaega
-                count += a.length - i; 
-                ans[k] = b[j];
-                j++;
-                k++;
+                // mtlb jth idx wala bnda, ith idx se, ith idx k end tak k sbhi bndo k saath inversion count bnaega
+                count[0] += a.length - i; 
+                ans[k++] = b[j++];
             }
         }
 
         while (i < a.length) {
-            ans[k] = a[i];
-            k++;
-            i++;
+            ans[k++] = a[i++];
         }
 
         while (j < b.length) {
-            ans[k] = b[j];
-            k++;
-            j++;
+            ans[k++] = b[j++];
         }
 
         return ans;
     }
     
 }
-
 //XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
