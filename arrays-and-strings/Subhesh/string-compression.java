3708e5ebbe27f3e8dc74c19c3dec585755a35cfb
Nidhi Kumari <nknidhi321@gmail.com>
2022-06-18 12:48:55 +0530

    Update string-compression.java

--- arrays-and-strings/Subhesh/string-compression.java
+++ arrays-and-strings/Subhesh/string-compression.java
@@ -1,35 +1,32 @@
 // https://leetcode.com/problems/string-compression/
 
 class Solution {
-
+    
     public int compress(char[] chars) {
         String str = String.valueOf(chars);
-        String ans = "";
-        int counter = 1;
-
-        for (int i = 1; i < str.length(); i++) {
-            if (str.charAt(i - 1) != str.charAt(i)) {
-                ans += str.charAt(i - 1); // computing ans of prev char(i-1) by standing at i
-                if (counter > 1) ans += counter;
-
+        int n = str.length();
+		String ans = "";
+		int counter = 1;
+		
+		for(int i = 1; i <= n; i++) { 
+		    if((i == n) || (str.charAt(i - 1) != str.charAt(i))) { // (i == n) handling the last character here only
+		        ans += str.charAt(i - 1); // computing ans of prev char(i-1) by standing at i
+		        if(counter > 1) ans += counter;
+		        
                 counter = 1; // setting counter for current character(i) to 1
-            } else { // str.charAt(i - 1) == str.charAt(i)
-                counter++; // simply increase counter
-            }
-        }
-
-        // handling the last character, bcoz i pe (i-1)th char ka answer nikalte hai hum
-        // So, last char bacha reh gaya tha waha
-        ans += str.charAt(str.length() - 1);
-        if (counter > 1) ans += counter;
-
-        // As given in the question, you have return your final ans in the input array itself
+		    }
+		    else {  // str.charAt(i - 1) == str.charAt(i)
+		        counter++;  // simply increase counter
+		    }
+		}
+        
+        // As given in the question, you have to return your final ans in the input array itself
         int i = 0;
-        for (char c: ans.toCharArray()) {
+        for(char c : ans.toCharArray()) {
             chars[i++] = c;
         }
-
+        
         return ans.length();
-    }
-
+	}
+    
 }
