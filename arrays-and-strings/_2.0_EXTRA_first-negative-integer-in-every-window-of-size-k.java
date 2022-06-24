280b0c50091d3ab41741c15a8b1e27b39200126c
Nidhi Kumari <nknidhi321@gmail.com>
2022-06-24 13:24:25 +0530

    Update _2.0_EXTRA_first-negative-integer-in-every-window-of-size-k.java

--- arrays-and-strings/_2.0_EXTRA_first-negative-integer-in-every-window-of-size-k.java
+++ arrays-and-strings/_2.0_EXTRA_first-negative-integer-in-every-window-of-size-k.java
@@ -1,23 +1,26 @@
-// https://practice.geeksforgeeks.org/problems/first-negative-integer-in-every-window-of-size-k3345/1/#
+// https://www.codingninjas.com/codestudio/problems/first-negative-integer-in-every-window-of-size-k_1164406?leftPanelTab=0
+// Don't use list, since removing from 0th idx would make shifting
+// Use queue instead
 
-class Compute {
-    
-    public long[] printFirstNegativeInteger(long arr[], int n, int K) {
+import java.util.*;
+
+public class Solution  {
+    public static ArrayList<Integer> firstNegativeInteger(ArrayList<Integer> arr, int K, int n) {
         int si = 0, ei = 0, idx = 0;
-        long[] ans = new long[n - K + 1];
-        List<Integer> list = new ArrayList<>(); // All Negative Element idx list
+        ArrayList<Integer> ans = new ArrayList<>();
+        Queue<Integer> queue = new LinkedList<>(); // All Negative Element idx list
         
         while(ei < n) {
-            if(arr[ei] < 0) list.add(ei); // Adding all negative ele idx in list  
+            if(arr.get(ei) < 0) queue.add(ei); // Adding all negative ele idx in list  
             
             if(ei - si + 1 == K) { // Agar window ki criteria me aare ho toh 
-                if(!list.isEmpty() && list.get(0) >= si) { // If list contains idx which is within my curr win   
-                    ans[idx++] = arr[list.get(0)]; // Form curr window ans
+                if(!queue.isEmpty() && queue.peek() >= si) { // If list contains idx which is within my curr win   
+                    ans.add(arr.get(queue.peek())); // Form curr window ans
                     
-                    if(list.get(0) == si) list.remove(0); // for the next upcoming widow
+                    if(queue.peek() == si) queue.poll(); // for the next upcoming widow
                 }
                 else {
-                    ans[idx++] = 0; // No -ve ele found in curr window
+                    ans.add(0); // No -ve ele found in curr window
                 }
                 si++;
             }
@@ -26,5 +29,4 @@ class Compute {
         }
         return ans;        
     }
-    
 }
