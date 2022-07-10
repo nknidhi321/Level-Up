1f7136bd3e0474871dabe649939d7ceb1c7cd0a1
Nidhi Kumari <nknidhi321@gmail.com>
2022-07-10 21:54:33 +0530

    Update _5.2_minimum-maximum-values-expression.java

--- dynamicProgramming/_5.2_minimum-maximum-values-expression.java
+++ dynamicProgramming/_5.2_minimum-maximum-values-expression.java
@@ -1,5 +1,55 @@
 // https://www.geeksforgeeks.org/minimum-maximum-values-expression/
 
+// Works for all digits number
+// Separated opertor and operand and used cut strategy
+// arr = [1,2,3,4,5]
+// op = "+*+*"
+
+// Consider only 2 numbers and observe cuts on operator
+// Here code is only for max, return pair for both min && max
+
+class HelloWorld {
+    public static void main(String[] args) {
+        String s = “1+2*3+4*5”;
+        System.out.println(MM(s));
+    }
+    
+    public static int MM(String s) {
+        String[] arr = s.split("[*+]"); // Segregating numbers
+        int n = arr.length;
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < s.length(); i++) {
+            if(s.charAt(i) == '*' || s.charAt(i) == '+') sb.append(s.charAt(i)); // Segregating operators
+        }
+        Integer[][] dp = new Integer[n][n];
+        return MM(0, n - 1, arr, sb.toString(), dp);
+    }
+    
+    public static int MM(int si, int ei, String[] arr, String op, Integer[][] dp) {
+        if(si == ei) return dp[si][ei] = Integer.parseInt(arr[si]);
+        
+        if(dp[si][ei] != null) return dp[si][ei];
+        
+        int max = Integer.MIN_VALUE;
+        for(int cut = si; cut < ei; cut++) { // Draw Tree diagram
+            int lc = MM(si, cut, arr, op, dp);
+            int rc = MM(cut + 1, ei, arr, op, dp);
+            
+            int myAns = 0;
+            if(op.charAt(cut) == '*') myAns = lc * rc; 
+            else myAns = lc + rc;
+            
+            max = Math.max(max, myAns);
+        }
+        return max;
+    } 
+    
+}
+
+//===================================================================================================================================================================
+// This code works only when there is single digit number/operand
+// Applied cut strategy on every operator without segregating operator and operand
+
     public static class pairmm {
         int min = (int) 1e9;
         int max = 0;
@@ -52,3 +102,5 @@
         System.out.println("Min value: " + res.min);
         System.out.println("Max value: " + res.max);
     }
+
+//===================================================================================================================================================================
