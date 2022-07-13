a076b7c911300276b04da033885f8fadb79bdb68
Nidhi Kumari <nknidhi321@gmail.com>
2022-07-13 14:46:35 +0530

    Update _3.6_inorder-successor-in-bst.java

--- binary-tree/_3.6_inorder-successor-in-bst.java
+++ binary-tree/_3.6_inorder-successor-in-bst.java
@@ -1,5 +1,39 @@
 // https://practice.geeksforgeeks.org/problems/inorder-successor-in-bst/1/
 
+// Travelling Left Root Right because, you have to find successor, basically iterating from start and the first value > num is your ans
+// Since u have to find successor, you can discard the left half of the tree by comparing it with the given num(boundary)
+// TC 
+// Average case : O(logN)
+// Worst Case : O(N) [A number beyond the min of the tree]
+
+public class Solution {
+   
+    public TreeNode inorderSuccessor(TreeNode root, TreeNode num) {
+        TreeNode[] ans = new TreeNode[] {null};
+        findInOrderSuccessor(root, num, ans);
+        return ans[0];
+    }
+    
+
+    boolean findInOrderSuccessor(TreeNode root, TreeNode num, TreeNode[] ans) {
+        if(root == null) return false;
+        
+        if(root.val > num.val) {  // Optimization // TC : O(logN)
+          if(findInOrderSuccessor(root.left, num, ans)) return true;
+        }
+        if(root.val > num.val) {
+          ans[0] = root;
+          return true;
+        }
+        if(findInOrderSuccessor(root.right, num, ans)) return true;
+        
+        return false;
+    }
+
+}
+
+---------------------------------------------------------------------------------------------------------
+    
 class Solution {
 
     public Node inorderSuccessor(Node root, Node k) {
