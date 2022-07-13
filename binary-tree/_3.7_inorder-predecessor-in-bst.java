3d4bcf82f907ad8bc6e6c62dec399ce727a621b0
Nidhi Kumari <nknidhi321@gmail.com>
2022-07-13 14:36:20 +0530

    Update _3.7_inorder-predecessor-in-bst.java

--- binary-tree/_3.7_inorder-predecessor-in-bst.java
+++ binary-tree/_3.7_inorder-predecessor-in-bst.java
@@ -1,5 +1,42 @@
 // https://www.lintcode.com/problem/915/
 
+// Travelling Right Root Left because, you have to find predecessor, basically iterating from end and the first value < num is your ans
+// Since u have to find predecessor, you can discard the right half of the tree by comparing it with the given num(boundary)
+// TC 
+// Average case : O(logN)
+// Worst Case : O(N) [A number beyond the max of the tree]
+
+public class Solution {
+ 
+    public TreeNode inorderPredecessor(TreeNode root, TreeNode num) {
+        TreeNode[] ans = new TreeNode[] {null};
+        findLargestSmallerKey(root, num, ans);
+        return ans[0];
+    }
+    
+
+    boolean findLargestSmallerKey(TreeNode root, TreeNode num, TreeNode[] ans) {
+        if(root == null) return false;
+        
+        if(root.val < num.val) {  // Optimization // TC : O(logN)
+          if(findLargestSmallerKey(root.right, num, ans)) return true;
+        }
+        if(root.val < num.val) {
+          ans[0] = root;
+          return true;
+        }
+        if(findLargestSmallerKey(root.left, num, ans)) return true;
+        
+        return false;
+    }
+
+}
+
+------------------------------------------------------------------------------------------------------------------
+
+// TC : O(N)
+// Morris inorder traversal
+    
 public class Solution {
 
     public TreeNode inorderPredecessor(TreeNode root, TreeNode p) {
