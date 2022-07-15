e302468b99344cb20fd4b28c3fdae2d88a48a0f8
Nidhi Kumari <nknidhi321@gmail.com>
2022-07-15 11:35:39 +0530

    Update and rename _2.7_palindrome-partitioning.java to _2.7_palindrome-partitioning-ii.java

--- dynamicProgramming/_2.7_palindrome-partitioning-ii.java
+++ dynamicProgramming/_2.7_palindrome-partitioning-ii.java
@@ -1,58 +1,4 @@
-// https://leetcode.com/problems/palindrome-partitioning/
-
-// Tabulation in pdp preparation + normal backtracking recursive solution for all palindrome substring 
-
-// Make sure to check the follow up at down !!
-
-class Solution {
-    
-    public static int n;
-    public static boolean[][] pdp;
-    
-    public List<List<String>> partition(String s) {
-        n = s.length();
-        pdp = new boolean[n][n];
-        preparePalindromeDP(s, pdp);
-        
-        List<List<String>> ans = new ArrayList<>();
-        partition_Rec(0, n - 1, s, new ArrayList<>(), ans);
-        return ans;
-    }
-    
-    // Tabulation palindrome dp of subtring 
-    public static void preparePalindromeDP(String s, boolean[][] pdp) {
-        for(int gap = 0; gap < n; gap++) {
-            for(int i = 0, j = gap; j < n; i++, j++) {
-                if(gap == 0) pdp[i][j] = true;
-                else if(gap == 1 && s.charAt(i) == s.charAt(j)) pdp[i][j] = true;
-                else pdp[i][j] = s.charAt(i) == s.charAt(j) && pdp[i + 1][j - 1];
-            }
-        }
-    }
-    
-    public static void partition_Rec(int si, int ei, String s, List<String> asf, List<List<String>> ans) {
-        if(si == n) {
-            ans.add(new ArrayList<>(asf));
-            return;
-        }
-        
-        // [si,ei] tak sabse poocho are you palindrome ? 
-        // If yes, add that palindrome substring your ans and make call for rest of the string
-        // Recursion ka path(option) is added in your answer
-        for(int cut = si; cut <= ei; cut++) {
-            if(pdp[si][cut]) { // Check in pdp if [si, cut] subtring is palindrome
-                asf.add(s.substring(si, cut + 1));  // Add in list
-                partition_Rec(cut + 1, ei, s, asf, ans);  // Making call to ros
-                asf.remove(asf.size() - 1);  // Backtrack
-            }
-        }
-    }
-        
-}
-
-//XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-
-// Follow up : https://leetcode.com/problems/palindrome-partitioning-ii/
+// https://leetcode.com/problems/palindrome-partitioning-ii/
 
 // Tabulation in pdp preparation + minimum cut based strategy for calulation of memo solution for all palindrome substring 
 // TC : O(N^3)
@@ -119,7 +65,7 @@ class Solution {
 // Tabulation in pdp preparation + minimum cut calulation memo solution for all palindrome substring 
 // Ditto as Palindrome Partitioning, here only difference is calculating minimum cut from all the recursive calls of cut
 // Find prefix palindrome, and check for rest of the string (ros)
-// So, here your left call is saved like we do in cut strategy, only right call goes, so O(N^2)
+// So, here your left call is saved like we do in cut strategy, only right call goes, only si is changing, so can be managed in 1D DP, so O(N^2)
 // tc :  O(N^2)
 
 class Solution {
@@ -148,20 +94,20 @@ class Solution {
     }
     
     public int minCut_Memo(int si, int ei, String s, Integer[] dp) {
-        if(si == n) return dp[si] = 0; 
         if(pdp[si][ei]) return dp[si] =  0;
         
         if(dp[si] != null) return dp[si];
 
-        // [si,ei] tak sabse poocho are you palindrome ? 
+        // [si,ei) tak sabse poocho are you palindrome ? 
         int minAns = (int) 1e8;
-        for(int cut = si; cut <= ei; cut++) {
+        for(int cut = si; cut < ei; cut++) {
             if(pdp[si][cut]) { // Checking in pdp if [si, cut] subtring is palindrome
                 minAns = Math.min(minAns, minCut_Memo(cut + 1, ei, s, dp)); // If yes, pass ros in next call
             }
         }
         return dp[si] = minAns + 1; // Taking minimum from all the cuts and hamara ek cut so +1
     }
+    
 }
 
-//XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+//XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
