a3a49f55bd2c9cd7b3fbd91959bc1f377ec94bf3
Nidhi Kumari <nknidhi321@gmail.com>
2022-07-18 02:45:28 +0530

    Update _1.6_decode-ways.java

--- dynamicProgramming/_1.6_decode-ways.java
+++ dynamicProgramming/_1.6_decode-ways.java
@@ -1,4 +1,41 @@
 // https://leetcode.com/problems/decode-ways/
+// Similar to No consecutive 0's 1's
+// Visualize DP like : No consecutive 0's 1's wala question, like iske piche jurna hai to kitne tareeke honge
+
+// Sumeet sir // Tabulation 
+// Follow this
+```
+class Solution {
+    
+    public int numDecodings(String s) {
+        if(s.charAt(0) == '0') return 0;
+        
+        int N = s.length();
+        int[] dp = new int[N + 1];  
+        return tab(s, N, dp);
+    }
+    
+    public int tab(String s, int N, int[] dp) {
+        dp[0] = dp[1] = 1; // 1 Tareeka jab string ki len 0 or 1 ho
+        for(int n = 2; n <= N; n++) {
+            int currDigit = s.charAt(n - 1) - '0';
+            if(currDigit != 0) { // Ex : 0 Invalid
+                dp[n] += dp[n - 1]; 
+            }
+            
+            int currDigitMinus1 = s.charAt(n - 2) - '0';
+            int twoDigit = (currDigitMinus1 * 10 + currDigit);    
+            if(currDigitMinus1 != 0 && twoDigit <= 26) { // Ex : 03 , 28 Invalid
+                dp[n] += dp[n - 2]; 
+            }
+        }
+        return dp[N];
+    }
+    
+}
+```
+
+----------------------------------------------------------------------------------------
 
 // Recursion TLE
 
