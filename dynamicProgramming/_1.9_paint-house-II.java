d0aab092d09062e0f7c9e6a5b0509159582b4e73
Nidhi Kumari <nknidhi321@gmail.com>
2022-07-19 14:38:33 +0530

    Update _1.9_paint-house-II.java

--- dynamicProgramming/_1.9_paint-house-II.java
+++ dynamicProgramming/_1.9_paint-house-II.java
@@ -31,3 +31,62 @@ public class Solution {
     }
 
 }
+
+//=========================================================================================================================================
+// Optimization TC : O(NC)
+// Using 4 extra var for the curr and prev row
+
+public class Solution {
+    
+    public int minCostII(int[][] arr) {
+        if(arr == null || arr.length == 0) return 0;
+        int houses = arr.length, colors = arr[0].length;
+        
+        // Cal min ans secMin for of 0th row
+        int min = Integer.MAX_VALUE, secMin = Integer.MAX_VALUE;
+        for(int color = 0; color < colors; color++) {
+            if(arr[0][color] < min) {
+                secMin = min;
+                min = arr[0][color];  
+            }
+            else if(arr[0][color] >= min && arr[0][color] <= secMin) {
+                secMin = arr[0][color];
+            }
+        }
+
+        // house 0 ko kisi v color se paint kar saktey ho
+        // So, arr k first row me jo hai wahi rehne do 
+        for(int house = 1; house < houses; house++) {           
+            int currMin = Integer.MAX_VALUE, currSecMin = Integer.MAX_VALUE;
+
+            for(int color = 0; color < colors; color++) {
+                // Forming ans for curr row
+                if(arr[house - 1][color] == min) arr[house][color] = secMin + arr[house][color];
+                else arr[house][color] = min + arr[house][color];
+
+                // Calculating min secMin for next row
+                // While forming the ans for the curr row
+                if(arr[house][color] < currMin) {
+                    currSecMin = currMin;
+                    currMin = arr[house][color];  
+                }
+                else if(arr[house][color] >= currMin && arr[house][color] <= currSecMin) {
+                    currSecMin = arr[house][color];
+                }
+            }
+            min = currMin; secMin = currSecMin;
+        }
+
+        // Printing DP
+        // for(int i = 0; i < arr.length; i++) {
+        //     for(int j = 0; j < arr[0].length; j++) {
+        //         System.out.print(arr[i][j] + "\t");
+        //     }
+        //     System.out.println();
+        // }
+        return min;
+    }
+
+}
+
+//=========================================================================================================================================
