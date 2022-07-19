1ff787495eea3aca42e1cbdc1d3da5cf349bec58
Nidhi Kumari <nknidhi321@gmail.com>
2022-07-19 20:14:17 +0530

    Update _1.9_painting-the-fence.java

--- dynamicProgramming/_1.9_painting-the-fence.java
+++ dynamicProgramming/_1.9_painting-the-fence.java
@@ -1,6 +1,30 @@
 // https://practice.geeksforgeeks.org/problems/painting-the-fence3727/1/#
 // Reference : https://www.youtube.com/watch?v=ju8vrEAsa3Q&t=864s
 
+/*
+    1 fence,  k = r g b
+    Color with any color, k choices
+    
+    =====================================
+    
+    2 fence
+    
+    1 2
+    
+    r r  3 choices
+    g g
+    b b
+    
+    r g  3 * 2 choices
+    r b
+    g r
+    g b
+    b r
+    b g
+    
+    Add them both for total ways
+*/
+
 class Solution {
     
     public long countWays(int n, int k) {
@@ -30,7 +54,37 @@ class Solution {
 
 //XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 // Optimization from the recurrence relation
+// New Code
 
+class Solution {
+    
+    long mod = (long)1e9 + 7;
+    
+    public long countWays(int n, int k) {
+        if(n == 1) return k;
+        if(n == 2) return (k + (k * (k - 1)) % mod) % mod;
+        
+		// Last 2 colors Ending at ii(same color)
+	    long same = k * 1; // repeation allowed upto 2 consecutive ele
+	    
+	    // Last 2 colors Ending at ij(diff color)
+	    long diff = k * (k - 1); // repeation not allowed 
+		
+        for(int i = 3; i <= n; i++) {
+            long currSame = diff * 1;
+            long currDiff = (((same + diff) % mod) * (k - 1)) % mod;
+            
+            same = currSame;
+            diff = currDiff;
+        }
+        return (same + diff) % mod;
+    }
+
+}
+
+//----------------------------------------------------------------------------
+
+// Old Code
 class Solution {
     
     public long countWays(int n, int k) {
