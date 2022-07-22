9704032fb1f83bbea042e1aa9ac3f1f3a33731bf
Nidhi Kumari <nknidhi321@gmail.com>
2022-07-23 01:14:29 +0530

    Update _2.10_word-break-ii.java

--- dynamicProgramming/_2.10_word-break-ii.java
+++ dynamicProgramming/_2.10_word-break-ii.java
@@ -9,40 +9,42 @@
 // Simple Recursion and Backtracking
 // No DP
 
+```
 class Solution {
     
-    public static int n;
+    int n;
     
-    public List<String> wordBreak(String s, List<String> wordDict) {
-        int maxLength = 0;
-        Set<String> dict = new HashSet<>();
-        for(String word : wordDict) {
-            maxLength = Math.max(maxLength, word.length());
-            dict.add(word);
-        }
+    public List<String> wordBreak(String s, List<String> arr) {
         n = s.length();
+        int maxWordLen = 0;
         List<String> ans = new ArrayList<>();
-        wordBreak(0, n - 1, maxLength, s, "", dict, ans);
+        Set<String> set = new HashSet<>();
+        
+        for(int i = 0; i < arr.size(); i++) {
+            set.add(arr.get(i));
+            maxWordLen = Math.max(maxWordLen, arr.get(i).length());
+        }
+        
+        wordBreak(0, n - 1, maxWordLen, s, set, "", ans);
         return ans;
     }
-    
-    public static void wordBreak(int si, int ei, int maxLength, String s, String ssf, Set<String> dict, List<String> ans) {
+        
+    public void wordBreak(int si, int ei, int maxWordLen, String s, Set<String> set, String ssf, List<String> ans) {
         if(si == n) {
             ans.add(ssf.trim());
             return;
         }
         
-        for(int brk = si; brk < si + maxLength && brk <= ei; brk++) {
-            String substr = s.substring(si, brk + 1);
-            if(dict.contains(substr)) { 
-                // Make sure you don't "assign" those changes in ssf, pass your changes in parameter 
-                wordBreak(brk + 1, ei, maxLength, s, ssf + substr + " ", dict, ans);
-                // No need to backtrack since using string,
+        for(int cut = si; cut <= Math.min(ei, si + maxWordLen - 1); cut++) {
+            String sub = s.substring(si, cut + 1);
+            if(set.contains(sub)) {
+                wordBreak(cut + 1, ei, maxWordLen, s, set, ssf + sub + " ", ans);
             }
         }
     }
     
 }
+```
 
 //XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 
