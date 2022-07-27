94407b9d019e0565d050523ff9fb416a65d0ea0d
Nidhi Kumari <nknidhi321@gmail.com>
2022-07-28 02:14:32 +0530

    Create image-multiplication.java

---
+++ binary-tree/Subhesh/image-multiplication.java
@@ -0,0 +1,16 @@
+// https://practice.geeksforgeeks.org/problems/image-multiplication0627/1
+
+ class Solution {
+     
+    long mod = (long)(1e9 + 7);
+    
+    public long imgMultiply(Node root) {
+        return ((long)(root.data * root.data) % mod + getAns(root.left, root.right)) % mod;         
+    }
+    
+    public long getAns(Node left, Node right) {
+        if(left == null || right == null) return 0;
+        return (((left.data * right.data) % mod) + (getAns(left.left, right.right) + getAns(left.right, right.left)) % mod) % mod;
+    }
+    
+}
