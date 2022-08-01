640f0ee27d6e6de99196c9f2c02f6c71ac241a93
Nidhi Kumari <nknidhi321@gmail.com>
2022-08-01 15:26:10 +0530

    Update house-robber.java

--- dynamicProgramming/_1_Extras/house-robber.java
+++ dynamicProgramming/_1_Extras/house-robber.java
@@ -1,10 +1,12 @@
 // https://leetcode.com/problems/house-robber/
 
-// Dp in kya store hoga ? 
-// Jab n number of houses hai toh maximum amount of money un n houses me se kya h wo dp[n] pe store hoga 
+/*
+    Dp in kya store hoga ? 
+    Jab n number of houses hai toh maximum amount of money un n houses me se kya h wo dp[n] pe store hoga 
+*/
 
 // Memoization
-
+```
 class Solution {
     
     public int rob(int[] nums) {
@@ -14,25 +16,26 @@ class Solution {
     }
     
     public static int rob_Memo(int n, int[] nums, Integer[] dp) {
-      
         // Jab house = 0 hai toh max 0 steal kar saktey ho
-        // Jab house = 1 hai toh max usi ek house ko steal kar sakey ho
-        if(n == 0 || n == 1) return dp[n] = (n == 0 ? 0 : nums[n - 1]);  
+        if(n == 0) return dp[0] = 0;  
         
         if(dp[n] != null) return dp[n];
         
         // Max of adjacent and currHouse se steal karne ka chance tvi milega jab house >= 2 honge
-        int adjacentMax = 0, currMax = 0;
-        adjacentMax = rob_Memo(n - 1, nums, dp);  // Not taking curr element
-        currMax = rob_Memo(n - 2, nums, dp) + nums[n - 1];  // Taking curr element
+        int adjacentMax = 0, adjacentsAdjacentMax = 0;
         
-        // Now, take the max of the above two choices
-        return dp[n] = Math.max(adjacentMax, currMax);
+        adjacentMax = rob_Memo(n - 1, nums, dp);   // Not taking curr element
+        if(n - 2 >= 0) adjacentsAdjacentMax = rob_Memo(n - 2, nums, dp);   // Taking curr element
+        
+		// Now, take the max of the above two choices
+        return dp[n] = Math.max(adjacentMax, adjacentsAdjacentMax + nums[n - 1]);
     }
+    
 }
-
-//XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX// Tabulation
-
+```
+-------------------------------------------------------------
+// Tabulation
+```
 class Solution {
     
     public int rob(int[] nums) {
@@ -43,25 +46,45 @@ class Solution {
     
     public static int rob_Tab(int N, int[] nums, Integer[] dp) {     
         for(int n = 0; n <= N; n++) {
-            if(n == 0 || n == 1) {
-                dp[n] = (n == 0 ? 0 : nums[n - 1]);
-                continue;
-            }
+            if(n == 0) {dp[0] = 0; continue;}
 
-            int adjacentMax = 0, currMax = 0;
+            int adjacentMax = 0, adjacentsAdjacentMax = 0;
             adjacentMax = dp[n - 1]; // rob_Memo(n - 1, nums, dp)
-            currMax = dp[n - 2] + nums[n - 1]; // rob_Memo(n - 2, nums, dp)
+            if(n - 2 >= 0) adjacentsAdjacentMax = dp[n - 2]; // rob_Memo(n - 2, nums, dp)
 
-            dp[n] = Math.max(adjacentMax, currMax);
+            dp[n] = Math.max(adjacentMax, adjacentsAdjacentMax  + nums[n - 1]);
         }
         return dp[N];
     }
 }
-
-//XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-
-// Optimization
-
+```
+-------------------------------------------------------------
+// Optimization 1 
+// Using Include exclude
+```
+class Solution {
+    
+    public int rob(int[] arr) {
+        int n = arr.length;
+        int incSum = arr[0];
+        int excSum = 0;
+        
+        for(int i = 1; i < n; i++) {
+            int newincSum = excSum + arr[i];
+            int newexcSum = Math.max(incSum, excSum);
+           
+            incSum = newincSum;
+            excSum = newexcSum;
+        }
+        return Math.max(incSum, excSum);
+    }
+    
+}
+```
+-------------------------------------------------------------
+// Optimization 2
+// Fibo
+```
 class Solution {
     
     public int rob(int[] nums) {
@@ -80,4 +103,44 @@ class Solution {
         return b;
     }
 }
-//XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+```
+-------------------------------------------------------------
+// Fibo optimization cannot be done if u use Pair class, and make calls to n - 1
+// Instead make calls to n - 1, and n - 2, for curr and adj max, like above codes.
+```
+class Solution {
+    
+    public static class Pair {
+        int withCurrRobbing;
+        int withoutCurrRobbing;
+        
+        public Pair(int withCurrRobbing, int withoutCurrRobbing) {
+            this.withCurrRobbing = withCurrRobbing;
+            this.withoutCurrRobbing = withoutCurrRobbing;
+        }
+    }
+    
+    public int rob(int[] arr) {
+        int n = arr.length;
+        Pair[] dp = new Pair[n + 1];
+        Pair p = maxAmount(n, arr, dp);
+        return Math.max(p.withCurrRobbing, p.withoutCurrRobbing);
+    }
+    
+    public Pair maxAmount(int n, int[] arr, Pair[] dp) {
+        if(n == 0) return dp[0] = new Pair(0, 0);
+        
+        if(dp[n] != null) return dp[n];
+        
+        Pair p = maxAmount(n - 1, arr, dp);
+        int withoutPrevRobbing = p.withoutCurrRobbing;
+        int withPrevRobbing = p.withCurrRobbing;
+        
+        int withCurrRobbing = withoutPrevRobbing + arr[n - 1];
+        int withoutCurrRobbing = Math.max(withoutPrevRobbing, withPrevRobbing);
+        return dp[n] = new Pair(withCurrRobbing, withoutCurrRobbing);
+    }
+    
+}
+```
+-------------------------------------------------------------
