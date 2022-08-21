78ddeee739a926ca8cd7aa3d203e3753540999d0
Nidhi Kumari <nknidhi321@gmail.com>
2022-08-21 13:26:42 +0530

    Update _6.6_number-of-ways-to-arrive-at-destination.java

--- graph/_6.6_number-of-ways-to-arrive-at-destination.java
+++ graph/_6.6_number-of-ways-to-arrive-at-destination.java
@@ -1,28 +1,31 @@
 // https://leetcode.com/problems/number-of-ways-to-arrive-at-destination/
 
+// Dijikstra
+// Updating dpCount ans while exploring neighbours
+// This is working because pq me saare cycle waale paths add ho he rahe hai,
+// bs updation dp array me ho raha hai
+// NOTE : This is "not without Cycle", this is "with Cycle"
+// Dijikstra will always require cycle and it is being taken care by the pq
+```
 class Solution {
     
     class Edge {
         int vtx;
-        int time;
-        public Edge(int vtx, int time) {
+        int w;
+        public Edge(int vtx, int w) {
             this.vtx = vtx;
-            this.time = time;
+            this.w = w;
         }
     }
     
-    class Pair implements Comparable<Pair>{
+    class Pair implements Comparable<Pair> {
         int vtx;
         int wsf;
-        int py;
-        String s = "";
-        public Pair(int vtx, int wsf, String s, int py) {
+        public Pair(int vtx, int wsf) {
             this.vtx = vtx;
             this.wsf = wsf;
-            this.s = s;
-            this.py = py;
         }
-        public int compareTo(Pair o){
+        public int compareTo(Pair o) {
             return this.wsf - o.wsf;
         }
     }
@@ -43,7 +46,6 @@ class Solution {
         
         int[] dis = new int[n];
         Arrays.fill(dis, Integer.MAX_VALUE);
-        dis[0] = 0;
 
         int[] dp = new int[n];
         dp[0] = 1;
@@ -51,54 +53,25 @@ class Solution {
         boolean[] vis = new boolean[n];
         
         PriorityQueue<Pair> pq = new PriorityQueue<>();
-        pq.add(new Pair(0, 0, ""+0, 1));
+        pq.add(new Pair(0, 0));
        
-       // boolean flag = false;
-       // int minTime = Integer.MAX_VALUE;
-        while(pq.size() != 0){
-            Pair rem = pq.remove();
-            //System.out.println(rem.s);
-            // System.out.println(rem.vtx);
-            // if(flag){
-            //     if(rem.vtx == n-1 && rem.wsf == minTime){
-            //        // System.out.println(rem.s);
-            //         count++;
-            //     }
-            // }
-            //if(rem.vtx == n - 1){
-                //System.out.println(rem.s);
-                
-            //}
-            // if(dis[rem.vtx] != -1) {
-            //     if(dis[rem.vtx] == rem.wsf){
-            //        // System.out.println(rem.vtx + " "+ rem.wsf + " " +dp[rem.py]);
-            //         if(rem.vtx == 0) System.out.println("Hello 0 if");
-            //         dp[rem.vtx] += dp[rem.py];
-            //     }
-            // }else{
-            //     if(rem.vtx == 0) System.out.println("Hello 0");
-            //     dis[rem.vtx] = rem.wsf;
-            //     dp[rem.vtx] += dp[rem.py];
-            // }
-            // dis[rem.vtx] = Math.min(dis[rem.vtx], rem.wsf);
-            // if(dis[rem.vtx] == rem.wsf) {
-            //     dp[rem.vtx] += 1;
-            // }
+        while(!pq.isEmpty()) {
+            Pair curr = pq.remove();
             
-            if(vis[rem.vtx]) continue;
+            if(vis[curr.vtx]) continue;
             
-            vis[rem.vtx] = true;
+            vis[curr.vtx] = true;
             
-            for(Edge nbr : graph[rem.vtx]) {               
+            for(Edge nbr : graph[curr.vtx]) {               
                 if(!vis[nbr.vtx]) {
-                    if(dis[nbr.vtx] > rem.wsf + nbr.time) {
-                        dis[nbr.vtx] = rem.wsf + nbr.time;
-                        dp[nbr.vtx] = (dp[rem.vtx]) % mod;
+                    if(dis[nbr.vtx] > curr.wsf + nbr.w) {
+                        dis[nbr.vtx] = curr.wsf + nbr.w;
+                        dp[nbr.vtx] = dp[curr.vtx];
                     }
-                    else if(dis[nbr.vtx] == rem.wsf + nbr.time) {
-                        dp[nbr.vtx] = (dp[nbr.vtx] + dp[rem.vtx]) % mod;
+                    else if(dis[nbr.vtx] == curr.wsf + nbr.w) {
+                        dp[nbr.vtx] = (dp[nbr.vtx] + dp[curr.vtx]) % mod;
                     }
-                    pq.add(new Pair(nbr.vtx, rem.wsf + nbr.time, rem.s + nbr.vtx, 0));
+                    pq.add(new Pair(nbr.vtx, curr.wsf + nbr.w));
                 }
             }
         }
@@ -111,3 +84,91 @@ class Solution {
     }
     
 }
+```
+
+---------------------------------------------------------------------------------------------------------
+
+// Dijikstra
+// Nikalte waqt dpCount ans bna rahe hai
+
+```
+class Solution {
+    
+    class Edge {
+        int vtx;
+        int w;
+        public Edge(int vtx, int w) {
+            this.vtx = vtx;
+            this.w = w;
+        }
+    }
+    
+    class Pair implements Comparable<Pair> {
+        int vtx;
+        int wsf;
+        int par;
+        public Pair(int vtx, int wsf, int par) {
+            this.vtx = vtx;
+            this.wsf = wsf;
+            this.par = par;
+        }
+        public int compareTo(Pair o) { // Sort in asc. order
+            return this.wsf - o.wsf;
+        }
+    }
+
+    int mod = (int)1e9 + 7;
+    
+    public int countPaths(int n, int[][] roads) {
+        ArrayList<Edge>[] graph = new ArrayList[n];
+        for(int i = 0; i < n; i++) graph[i] = new ArrayList<>();
+        
+        for(int[] road : roads) {
+            int u = road[0];
+            int v = road[1];
+            int w = road[2];
+            graph[u].add(new Edge(v, w));
+            graph[v].add(new Edge(u, w));
+        }
+        
+        int[] dis = new int[n];
+        Arrays.fill(dis, (int)1e9);
+
+        int[] dp = new int[n];
+        boolean[] vis = new boolean[n];
+        
+        PriorityQueue<Pair> pq = new PriorityQueue<>();
+        pq.add(new Pair(0, 0, -1));
+       
+        while(!pq.isEmpty()) {
+            Pair curr = pq.remove();
+            
+            if(dis[curr.vtx] == (int)1e9) { // Pehli baar nikla, min wt. k saath he nikalega
+                dis[curr.vtx] = curr.wsf;
+                if(curr.par == -1) dp[curr.vtx] = 1; // 0th node ka 1 rasta hai
+                else dp[curr.vtx] = dp[curr.par]; // Any other node
+            }
+            else if(dis[curr.vtx] == curr.wsf) { // Agar is min wt. k jitna aur wt. hai, update ans
+                dp[curr.vtx] = (dp[curr.par] + dp[curr.vtx]) % mod;
+            }
+            
+            if(vis[curr.vtx]) continue;
+            
+            vis[curr.vtx] = true;
+            for(Edge nbr : graph[curr.vtx]) {               
+                if(!vis[nbr.vtx]) {
+                    pq.add(new Pair(nbr.vtx, curr.wsf + nbr.w, curr.vtx));
+                }
+            }
+        }
+        
+        // for(int i = 0; i < n; i++) {
+        //    System.out.println(i + "-> cost-> " + dis[i] + " ways-> " + dp[i]);
+        // }
+        
+        return dp[n - 1];
+    }
+    
+}
+```
+--------------------------------------------------------------------------------------------------------
