e906136d1421b21f455ce09352bfd6b43faff060
Nidhi Kumari <nknidhi321@gmail.com>
2022-08-26 14:13:20 +0530

    Update _1.1_detect-cycle-in-a-directed-graph.java

--- graph/_1.1_detect-cycle-in-a-directed-graph.java
+++ graph/_1.1_detect-cycle-in-a-directed-graph.java
@@ -1,5 +1,6 @@
 // https://practice.geeksforgeeks.org/problems/detect-cycle-in-a-directed-graph/1
 
+// GFG
 class Solution {
 
     public boolean isCyclic(int V, ArrayList<ArrayList<Integer>> adj) {
@@ -36,6 +37,40 @@ class Solution {
         
         recStack[i] = false; // backtrack
   
+        return false;
+    }
+    
+}
+
+//================================================================================================================================================
+
+// Shamii
+class Solution {
+
+    public boolean isCyclic(int V, ArrayList<ArrayList<Integer>> adj) {
+        boolean[] begin = new boolean[V];
+        boolean[] end = new boolean[V];
+        for(int i = 0; i < V; i++) {
+            if(isCyclicUtil(i, begin, end, adj)) {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    private boolean isCyclicUtil(int i, boolean[] begin, boolean[] end, ArrayList<ArrayList<Integer>> adj) {
+        if(begin[i] && end[i]) return false; 
+        if(begin[i] && !end[i]) return true;    
+              
+        begin[i] = true;
+        List<Integer> children = adj.get(i);
+        for(Integer c: children) {
+            if(isCyclicUtil(c, begin, end, adj)) {
+                return true;
+            }
+        }                  
+        end[i] = true;
+
         return false;
     }
     
