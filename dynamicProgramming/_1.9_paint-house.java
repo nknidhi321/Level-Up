33401c8a31cc045dbc1f1628e4ee8d7539b0b392
Nidhi Kumari <nknidhi321@gmail.com>
2022-08-31 14:36:00 +0530

    Update _1.9_paint-house.java

--- dynamicProgramming/_1.9_paint-house.java
+++ dynamicProgramming/_1.9_paint-house.java
@@ -1,5 +1,6 @@
 // https://www.lintcode.com/problem/515/
 
+// Tabulation
 public class Solution {
  
     public int minCost(int[][] cost) {
@@ -13,4 +14,42 @@ public class Solution {
         }
         return Math.min(cost[n - 1][0], Math.min(cost[n - 1][1], cost[n - 1][2]));
     }
+}
+
+//---------------------------------------------------------------------------------------
+
+// Recursively
+// TLE
+public class Solution {
+    
+    class Triplet {
+        int A;
+        int B;
+        int C;
+        Triplet(int A, int B, int C) {
+            this.A = A;
+            this.B = B;
+            this.C = C;
+        }
+    }
+
+    public int minCost(int[][] costs) {
+        int n = costs.length;
+        if(costs == null || n == 0) return 0;
+
+        Triplet tri = minCost(n, costs);
+        return Math.min(tri.C, Math.min(tri.A, tri.B));
+    }
+
+    public Triplet minCost(int n, int[][] costs) {
+        if(n == 1) return new Triplet(costs[0][0], costs[0][1], costs[0][2]);
+        
+        Triplet tri = minCost(n - 1, costs);
+        int A = Math.min(tri.B, tri.C) + costs[n - 1][0];
+        int B = Math.min(tri.A, tri.C) + costs[n - 1][1];
+        int C = Math.min(tri.A, tri.B) + costs[n - 1][2];
+        System.out.println(n);
+        return new Triplet(A, B, C);
+    }
+
 }
