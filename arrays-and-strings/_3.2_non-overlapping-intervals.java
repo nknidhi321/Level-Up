287b51f514fc2098a09849988cc62cb6424d0202
Nidhi Kumari <nknidhi321@gmail.com>
2022-09-13 09:13:00 -0700

    Update _3.2_non-overlapping-intervals.java

--- arrays-and-strings/_3.2_non-overlapping-intervals.java
+++ arrays-and-strings/_3.2_non-overlapping-intervals.java
@@ -1,6 +1,53 @@
 // https://leetcode.com/problems/non-overlapping-intervals/
 
-// Ulta of https://leetcode.com/problems/minimum-number-of-arrows-to-burst-balloons/
+/*
+        The idea is to sort all intervals on starting pt. 
+        Now, compare the intervals, while chosing any interval
+        always keep the interval with the least ending pt.[discard the interval with the greater ending pt.],
+        so that it has less possibility of overlapping with the upcoming intervals
+
+
+        1)       -----------------------    Discard this larger interval
+                        --------            => Lesser ending pt. [Keep this]
+
+
+        2)        -------------------       => Lesser ending pt. [Keep this]
+                                ----------  Discard this interval 
+*/
+
+
+class Solution {
+    
+    public int eraseOverlapIntervals(int[][] intervals) {
+        int n = intervals.length;
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]); // sort on starting pt.
+        
+        int start = intervals[0][0];
+        int end = intervals[0][1];
+        
+        int count = 0;
+        for(int i = 1; i < n; i++) {
+            int currStart = intervals[i][0];
+            int currEnd = intervals[i][1];
+            if(end > currStart) { // overlapping
+                count++;
+                end = Math.min(end, currEnd); // jiski ending pt. kam h usko rakh lo
+            }
+            else { // non overlapping
+                start = currStart;
+                end = currEnd;
+            }
+        }
+        return count;
+    }
+    
+}
+
+
+//================================================================================================================
+
+// Using same technique of bus Ulta of https://leetcode.com/problems/minimum-number-of-arrows-to-burst-balloons/
+// Complicated // Ignore
 
 class Solution {
     
