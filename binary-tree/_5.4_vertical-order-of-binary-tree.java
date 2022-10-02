6aeda827c1ef509662aae6af3d6c823f3acea227
Nidhi Kumari <nknidhi321@gmail.com>
2022-10-02 01:22:46 -0700

    Update _5.4_vertical-order-of-binary-tree.java

--- binary-tree/_5.4_vertical-order-of-binary-tree.java
+++ binary-tree/_5.4_vertical-order-of-binary-tree.java
@@ -76,34 +76,22 @@ class Solution {
 
 // Follow up :-
 // https://leetcode.com/problems/vertical-order-traversal-of-a-binary-tree/
-// If nodes are on same hl && vl then priority should be given in smaller order while printing
+// If nodes are on same hl && vl then priority should be given to the node with smaller val while printing
 // Use priorityQueue and implement Comparable in Pair class
 
 class Solution {
-
-    public static class Pair implements Comparable<Pair> {
+    class Pair implements Comparable<Pair> {
         TreeNode TreeNode;
         int hl;
         int vl; // vertical length or width
-        
         public Pair(TreeNode TreeNode, int hl, int vl) {
             this.TreeNode = TreeNode;
             this.hl = hl;
             this.vl = vl;
         }
-        
-        public int compareTo(Pair o) {
-            if((this.hl - o.hl) == 0) {
-                if((this.vl - o.vl) == 0) {
-                    return this.TreeNode.val - o.TreeNode.val;
-                }
-                else {
-                    return this.vl - o.vl;
-                }
-            }
-            else {
-                return this.hl - o.hl;
-            }
+        public int compareTo(Pair o) { // Agar hl same h toh val k basis pe chote bnde ko mangwao
+            if((this.hl - o.hl) == 0) return this.TreeNode.val - o.TreeNode.val;
+            else return this.hl - o.hl;
         }
     }
     
@@ -116,7 +104,7 @@ class Solution {
         
         int len = minMax[1] - minMax[0] + 1;
         
-        // ArrayList<ArrayList<Integer>> will contain all vertical vl as index of ArrayList and corresponding vertical list of TreeNode values
+        // ans will contain all vertical vl as index of ArrayList and corresponding vertical list of TreeNode values
         // As it is ArrayList make sure your vl starts from 0 and not < 0
         List<List<Integer>> ans = new ArrayList<>();    
         for(int i = 0; i < len; i++) ans.add(new ArrayList<>());  // Intialize arrayList of that minMax width
@@ -125,19 +113,14 @@ class Solution {
         queue.add(new Pair(root, 0, Math.abs(minMax[0])));  // Why minMax[0] ? To shift origin, make the least negative idx as 0th idx and so on...       // IMPORTANT
         
          while(!queue.isEmpty()) {
-            int size = queue.size();
-            
-            while(size-- > 0) {
-                Pair pair = queue.remove();
-                ans.get(pair.vl).add(pair.TreeNode.val);
-                
-                if(pair.TreeNode.left != null) queue.add(new Pair(pair.TreeNode.left, pair.hl + 1, pair.vl - 1));
-                if(pair.TreeNode.right != null) queue.add(new Pair(pair.TreeNode.right, pair.hl + 1, pair.vl + 1));
-            }
+            Pair pair = queue.remove();
+            ans.get(pair.vl).add(pair.TreeNode.val);
+
+            if(pair.TreeNode.left != null) queue.add(new Pair(pair.TreeNode.left, pair.hl + 1, pair.vl - 1));
+            if(pair.TreeNode.right != null) queue.add(new Pair(pair.TreeNode.right, pair.hl + 1, pair.vl + 1));
         }
         return ans;
     }
-    
   
     // Calculate width / shadow
     public static void widthOfShadow(TreeNode root, int[] minMax, int idx) {
