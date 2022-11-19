ee8351717e11776d85732980ca299da9ee3b6b00
Nidhi Kumari <nknidhi321@gmail.com>
2022-11-19 10:45:15 +0530

    Update _Archit_2.3_3sum-closest.java

--- TwoPointer/_Archit_2.3_3sum-closest.java
+++ TwoPointer/_Archit_2.3_3sum-closest.java
@@ -4,40 +4,29 @@
 
 class Solution {
     
-    public static int n;
-    
-    public int threeSumClosest(int[] nums, int target) {
-        n = nums.length;
-        Arrays.sort(nums);
+    public int threeSumClosest(int[] arr, int target) {
+        int n = arr.length, ans = -1; 
+        int min = Integer.MAX_VALUE;
+        Arrays.sort(arr);
         
-        int ans = 0, min = Integer.MAX_VALUE;
-        for(int i = 0; i <= n - 3; i++) { // Fixing the ith element and calling 2 pointer from [i+1 to n-1]
-            int twoSumClosest = twoSumClosest(i + 1, n - 1, target - nums[i], nums);
-            int threeSumClosest = nums[i] + twoSumClosest;
+        // 3 sum
+        for(int start = 0; start <= n - 3; start++) {
+            int smallTar = target - arr[start];
+            int i = start + 1, j = n - 1;
             
-            int diff = Math.abs(threeSumClosest - target);  // To find the closeness
-            if(diff < min) {
-                min = diff;
-                ans = threeSumClosest;
-            }
-        }  
-        return ans;
-    }
-    
-    
-    public int twoSumClosest(int start, int end, int target, int[] nums) {
-        int ans = 0, min = Integer.MAX_VALUE;
-        
-        // Two pointer
-        while(start < end) {
-            int sum = nums[start] + nums[end];
-            if(sum < target) start++;
-            else end--;
-            
-            int diff = Math.abs(sum - target); // To find the closeness
-            if(diff < min) {
-                min = diff;
-                ans = sum;
+            // 2 sum
+            while(i < j) {
+                int sum = arr[i] + arr[j];
+                if(sum < smallTar) i++;
+                else if(sum > smallTar) j--;
+                else return sum + arr[start]; // found closest
+                
+                // compete ans
+                int diff = Math.abs(target - (sum + arr[start]));
+                if(diff < min) { 
+                    min = diff;
+                    ans = sum + arr[start];
+                }
             }
         }
         return ans;
@@ -45,4 +34,4 @@ class Solution {
     
 }
 
-//XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+//XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
