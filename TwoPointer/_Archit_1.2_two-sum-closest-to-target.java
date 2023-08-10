ef4015d43bd9afc291dcdef4b0cfd5303eceab6c
Nidhi Kumari <nknidhi321@gmail.com>
2023-08-10 12:48:46 +0530

    Update _Archit_1.2_two-sum-closest-to-target.java

--- TwoPointer/_Archit_1.2_two-sum-closest-to-target.java
+++ TwoPointer/_Archit_1.2_two-sum-closest-to-target.java
@@ -1,6 +1,18 @@
 // https://www.lintcode.com/problem/533/description
 // TC : nlogn
+/*
+        Given an int array nums and an int target. Find two integers in nums such that the sum is closest to target.
+        Example 1:
+        
+        Input: nums = [1, 2, 3, 4, 5], target = 10
+        Output: [4, 5]
+        Example 2:
+        
+        Input: nums= [-1, 2, 1, -4], target = 4
+        Output: [2, 1]
+*/
 
+    
 public class Solution {
 
     public int twoSumClosest(int[] nums, int target) {
