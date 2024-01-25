87e96264a6ba262148e149d3fea18dd707897043
Nidhi Kumari <nknidhi321@gmail.com>
2024-01-25 21:53:28 +0530

    Update _2.5_pacific-atlantic-water-flow.java

--- graph/_2.5_pacific-atlantic-water-flow.java
+++ graph/_2.5_pacific-atlantic-water-flow.java
@@ -1,5 +1,66 @@
 // https://leetcode.com/problems/pacific-atlantic-water-flow/
+
 // DFS
+// Making DFS calls only from the boundary cells
+// Take 2 separate matrix one for atlantic and one for pacific
+// Now take out the commons 
+
+class Solution {
+    int row;
+    int col;
+    int dir[][];
+    
+    public List<List<Integer>> pacificAtlantic(int[][] heights) {
+        row = heights.length; // row
+        col = heights[0].length; // col
+        List<List<Integer>> ans = new ArrayList<>();
+        dir = new int[][] {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+        int[][] visAtlantic = new int[row][col];
+        int[][] visPacific = new int[row][col];
+        
+        // row and col is different so making 2 loops separately
+        // left and right boundary
+        for(int i = 0; i < row; i++) {
+            DFS(i, 0, visPacific, heights); // from left(pacific) make DFS
+            DFS(i, col - 1, visAtlantic, heights); // from right(atlantic) make DFS
+        }
+        // top and bottom boundary
+        for(int i = 0; i < col; i++) {
+            DFS(0, i, visPacific, heights); // from top(pacific) make DFS
+            DFS(row - 1, i, visAtlantic, heights); // from bottom(atlantic) make DFS
+        }
+        
+        // now check common cell marked in both visAtlantic && visPacific
+        for(int i = 0; i < row; i++) {
+            for(int j = 0; j < col; j++) {
+                List<Integer> cell = new ArrayList<>();
+                if(visAtlantic[i][j] == 1 && visPacific[i][j] == 1) {
+                    cell.add(i);
+                    cell.add(j);
+                    ans.add(cell);
+                }
+            }
+        }
+        return ans;
+    }
+    
+    // simple DFS
+    public void DFS(int i, int j, int[][] visited, int[][] heights) {
+        visited[i][j] = 1;
+        for(int d = 0; d < dir.length; d++) {
+            int r = i + dir[d][0];
+            int c = j + dir[d][1];
+            if((r >= 0 && c >= 0 && r < row && c < col && visited[r][c] != 1 && heights[r][c] >= heights[i][j])) {
+                DFS(r, c, visited, heights);
+            }
+        }
+    }
+    
+}
+
+---------
+
+// O(n * m) ^ 2
 // From every cell make DFS call and keep track if are going out of boundary
 // from (i < 0 || j < 0) it's pacific ocean and if (i > row || j > col) then it's pacific ocean
 // Just store that you found atlantic or pacific
