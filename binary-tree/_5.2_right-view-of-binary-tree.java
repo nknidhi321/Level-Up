bfbc3495d4ce0d5caaef4a72a81f78e6cf465345
Nidhi Kumari <nknidhi321@gmail.com>
2024-04-26 18:21:22 +0530

    Update _5.2_right-view-of-binary-tree.java

--- binary-tree/_5.2_right-view-of-binary-tree.java
+++ binary-tree/_5.2_right-view-of-binary-tree.java
@@ -1,8 +1,7 @@
 // https://leetcode.com/problems/binary-tree-right-side-view/
-// Do level order traversal and add right child first in your queue, so the 1st element popped at every level is your answer
 
+// Do level order traversal and add right child first in your queue, so the 1st element popped at every level is your answer
 class Solution {
-    
     public List<Integer> rightSideView(TreeNode root) {
         if(root == null) return new ArrayList<>();
                
@@ -22,5 +21,53 @@ class Solution {
             }
         }
         return ans;    
+    }
+}
+
+----------------------------------------------------------------------------------------------------------------------
+
+// Here we are travelling : root, left, right
+// Using PreOrder traversal : root, left, right
+
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> ans = new ArrayList<>();
+        rightSideView(root, 0, ans);
+        System.out.println(ans);
+        return ans;
+    }
+    
+    public static void rightSideView(TreeNode root, int level, List<Integer> ans) {
+        if(root == null) return;
+        
+        if(ans.size() == level) ans.add(root.val); //visiting level for the 1st time
+        else ans.set(level, root.val); // already visited level, replace that index/level value
+        
+        rightSideView(root.left, level + 1, ans);
+        rightSideView(root.right, level + 1, ans);
+    }
+}
+
+------------------------------------------------------------------------------------------------------------------------
+
+// Here we are travelling : root, right, left
+// Note this is not PostOrder trversal 
+// In PostOrder trversal we travel like left, right, root
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> ans = new ArrayList<>();
+        rightSideView(root, 0, ans);
+        System.out.println(ans);
+        return ans;
+    }
+    
+    public static void rightSideView(TreeNode root, int level, List<Integer> ans) {
+        if(root == null) return;
+        
+        if(ans.size() == level) ans.add(root.val); //visiting level for the 1st time
+        
+        rightSideView(root.right, level + 1, ans);
+        rightSideView(root.left, level + 1, ans);
+        
     }
 }
