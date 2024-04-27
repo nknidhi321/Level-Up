b498b0bc0cf0945f8ffef630b10dfa4c598359d2
Nidhi Kumari <nknidhi321@gmail.com>
2024-04-27 15:48:27 +0530

    Update _1.1_fibonacci-number.java

--- dynamicProgramming/_1.1_fibonacci-number.java
+++ dynamicProgramming/_1.1_fibonacci-number.java
@@ -1,5 +1,6 @@
 // https://leetcode.com/problems/fibonacci-number/
 
+// Memoization
 class Solution {
     public int fib(int n) {
         Integer[] dp = new Integer[n + 1];
@@ -15,5 +16,28 @@ class Solution {
         if(n - 1 >= 0) sum += fibo(n - 1, dp);   
         if(n - 2 >= 0) sum += fibo(n - 2, dp);
         return dp[n] = sum;
+    }
+}
+
+-------------------------------------------------
+// Tabulation 
+class Solution {
+    public int fib(int n) {
+        Integer[] dp = new Integer[n + 1];
+        return fibo(n, dp);
+    }
+    public int fibo(int N, Integer[] dp) {
+        for(int n = 0; n <= N; n++) {
+            if(n == 0 || n == 1) {
+                dp[n] = n;
+                continue;
+            }
+
+            int sum = 0;
+            if(n - 1 >= 0) sum += dp[n - 1]; // fibo(n - 1, dp);   
+            if(n - 2 >= 0) sum += dp[n - 2]; // fibo(n - 2, dp);
+            dp[n] = sum;
+        }
+        return dp[N];
     }
 }
