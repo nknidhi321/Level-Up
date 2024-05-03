8467c40ea2bfb4ec8bba62ac9f15e1454d5cd2f2
Nidhi Kumari <nknidhi321@gmail.com>
2024-05-03 15:09:33 +0530

    Update x-of-a-kind-in-a-deck-of-cards-OR-GCD.java

--- arrays-and-strings/x-of-a-kind-in-a-deck-of-cards-OR-GCD.java
+++ arrays-and-strings/x-of-a-kind-in-a-deck-of-cards-OR-GCD.java
@@ -2,7 +2,13 @@
 // https://www.youtube.com/watch?v=UvpXInRkZ3Q
 // https://www.youtube.com/watch?v=ZtDeDD1VYLk
 
-// GCD will be the total count of individual group member
+
+// NOTE : 
+// gcd of any number with 0 is that number itself
+// gcd of 0 with any number is that number itself
+// gcd(a, b) == gcd(b, a)
+
+// Intuition : GCD will be the total count of individual group member
 class Solution {
     
     public boolean hasGroupsSizeX(int[] deck) {
@@ -11,17 +17,19 @@ class Solution {
         for(int i = 0; i < deck.length; i++) {
             freq[deck[i]]++;
         }
-        
-        int ans = 0; // gcd of any number with 0 is that number itself
+
+        int ans = 0;
         for(int i = 0; i <= 1000; i++) {
-            if(freq[i] != 0) {
+            if(freq[i] != 0) { // Anyway handled here n1 will never become 0
                 ans = gcd(freq[i], ans);
             }
         }
         return ans > 1 ? true : false;
     }
-    
+
+    // if n1 becomes 0 here, handle it as edge case because it will throw Arithmetic Exception
     // public int gcd(int n1, int n2) {
+    //     if(n1 == 0) return n2;
     //     while(n2 % n1 != 0) {
     //         int rem = n2 % n1;
     //         n2 = n1;
@@ -29,7 +37,9 @@ class Solution {
     //     }
     //     return n1;
     // }
+
     
+    // if b becomes 0 here, it will handle that scenario in base case
     public int gcd(int a, int b) {
         if(b == 0) return a;
         return gcd(b, a % b);
