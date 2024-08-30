63ee769fd1ce575d758c1f6bff12aaa025e81e0a
Nidhi Kumari <nknidhi321@gmail.com>
2024-08-30 21:13:06 +0530

    Update power-of-numbers.java

--- recursion/power-of-numbers.java
+++ recursion/power-of-numbers.java
@@ -1,6 +1,19 @@
 // https://www.geeksforgeeks.org/problems/power-of-numbers-1587115620/1
 
-// Find N ^ R
+// Find N ^ R 
+
+// If b is
+// Even : a^b = a^b/2 * a^b/2
+// Odd : a^b = a^b/2 * a^b/2 * a
+
+// Example : 
+// a^32 = a^16 * a^16
+// a^16 = a^8 * a^8
+// a^8 = a^4 * a^4
+// a^4 = a^2 * a^2
+// a^2 = a^1 * a^1
+// a^1 = a^0 * a^0 * a
+
 
 class Solution {
     
