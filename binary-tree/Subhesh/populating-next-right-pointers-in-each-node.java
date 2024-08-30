0a2814a585a6e3cc4257ac110661898c4455ad11
Nidhi Kumari <nknidhi321@gmail.com>
2024-08-30 15:28:18 +0530

    Update populating-next-right-pointers-in-each-node.java

--- binary-tree/Subhesh/populating-next-right-pointers-in-each-node.java
+++ binary-tree/Subhesh/populating-next-right-pointers-in-each-node.java
@@ -1,23 +1,44 @@
 // https://leetcode.com/problems/populating-next-right-pointers-in-each-node/
 
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+    
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
+*/
+
 class Solution {
     
     public Node connect(Node root) {
         if(root == null) return root;
-    
+        
         LinkedList<Node> queue = new LinkedList<>();
         queue.add(root);
-        
         while(!queue.isEmpty()) {
             int size = queue.size();
-            while(size > 0) { // Ek iteration me 2 nodes nikalna is wrong approach kuki 2nd wale bnde ko v toh koi point krna chahiye
-                Node node1 = queue.poll(); // So, that's why peek krwa lo 2nd bnde ko and 1st k piche lga do
-                size--;
-                Node node2 = size == 0 ? null : queue.peek(); // "Is level" k queue me agar sirf 1 he bnda tha toh wo null ko point karega
-                node1.next = node2;
+            while(size-- > 0) {
+                Node node = queue.poll();
+                if(size == 0) node.next = null; // "Is level" k queue me agar sirf 1 he bnda tha toh wo null ko point karega
+                else node.next = queue.peek();
                 
-                if(node1.left != null) queue.add(node1.left);
-                if(node1.right != null) queue.add(node1.right);
+                if(node.left != null) queue.add(node.left);
+                if(node.right != null) queue.add(node.right);
             }
         }
         return root;
