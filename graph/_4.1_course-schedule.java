cfc9b0d7c28dc0b8aeb12d0ac94a1c094d2471f4
Nidhi Kumari <nknidhi321@gmail.com>
2024-09-26 21:08:59 +0530

    Update _4.1_course-schedule.java

--- graph/_4.1_course-schedule.java
+++ graph/_4.1_course-schedule.java
@@ -1,5 +1,28 @@
 // https://leetcode.com/problems/course-schedule/
 
+/*
+There are a total of numCourses courses you have to take, labeled from 0 to numCourses - 1.
+You are given an array prerequisites where prerequisites[i] = [ai, bi] 
+indicates that you must take course bi first if you want to take course ai.
+
+For example, the pair [0, 1], indicates that to take course 0 you have to first take course 1.
+Return true if you can finish all courses. Otherwise, return false.
+
+Example 1:
+Input: numCourses = 2, prerequisites = [[1,0]]
+Output: true
+Explanation: There are a total of 2 courses to take. 
+To take course 1 you should have finished course 0. So it is possible.
+
+Example 2:
+Input: numCourses = 2, prerequisites = [[1,0],[0,1]]
+Output: false
+Explanation: There are a total of 2 courses to take. 
+To take course 1 you should have finished course 0, and to take course 0 you should also have finished course 1. 
+So it is impossible.
+*/
+
+
 // Rajneesh
 // By creating graph
 
@@ -52,7 +75,7 @@ class Solution {
     } 
 }
 
-----------------------------------------------------------------------------------
+// ----------------------------------------------------------------------------------
     
 // Mine
 // Same but Just u v difference
@@ -97,9 +120,9 @@ class Solution {
 
 //XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 
-//Mine
-//Same as above but without creating graph
-//Not efficient since travelling over all matrix to find neigh for a vtx 
+// Mine
+// Same as above but without creating graph
+// Not efficient since travelling over all matrix to find neigh for a vtx 
 
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
@@ -133,4 +156,5 @@ class Solution {
         return nodeCount == numCourses ? true : false;
     } 
 }
+
 //XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
